a0fbeec9530733c60e47a55d312dbadbff6918a1
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-01-25 12:32:10 +0000

    issue INDISTAC-21: Configuración inicial del proyecto
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@32917 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ indicators-core/src/main/java/es/gobcan/istac/indicators/core/service/impl/IndicatorsSystemServiceImpl.java
@@ -0,0 +1,24 @@
+package es.gobcan.istac.indicators.core.service.impl;
+
+import org.fornax.cartridges.sculptor.framework.errorhandling.ApplicationException;
+import org.fornax.cartridges.sculptor.framework.errorhandling.ServiceContext;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Implementation of IndicatorsSystemService.
+ */
+@Service("indicatorsSystemService")
+public class IndicatorsSystemServiceImpl extends IndicatorsSystemServiceImplBase {
+    public IndicatorsSystemServiceImpl() {
+    }
+
+    public void createIndicatorSystem(ServiceContext ctx)
+        throws ApplicationException {
+
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException(
+            "createIndicatorSystem not implemented");
+
+    }
+}
