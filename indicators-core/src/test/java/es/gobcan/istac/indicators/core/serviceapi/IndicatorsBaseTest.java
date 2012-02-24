2e18d2010c0077c52cf66b9784713c3c05d5a086
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-02-24 09:30:29 +0000

    Todas los nombres de tablas en plural
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@33622 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-core/src/test/java/es/gobcan/istac/indicators/core/serviceapi/IndicatorsBaseTest.java
+++ indicators-core/src/test/java/es/gobcan/istac/indicators/core/serviceapi/IndicatorsBaseTest.java
@@ -12,13 +12,13 @@ public abstract class IndicatorsBaseTest extends MetamacBaseTests {
         List<String> tables = new ArrayList<String>();
         tables.add("TBL_LOCALISED_STRINGS");
         tables.add("TBL_ELEMENTS_LEVELS");
-        tables.add("TBL_INDICATOR_INSTANCES");
+        tables.add("TBL_INDICATORS_INSTANCES");
         tables.add("TBL_DIMENSIONS");
         tables.add("TBL_INDIC_SYSTEMS_VERSIONS");
         tables.add("TBL_INDICATORS_SYSTEMS");
-        tables.add("TBL_DATA_SOURCE_VARIABLES");
+        tables.add("TBL_DATA_SOURCES_VARIABLES");
         tables.add("TBL_DATA_SOURCES");
-        tables.add("TBL_INDICATOR_VERSIONS");
+        tables.add("TBL_INDICATORS_VERSIONS");
         tables.add("TBL_INDICATORS");
         tables.add("TBL_INTERNATIONAL_STRINGS");
         return tables;
@@ -33,10 +33,10 @@ public abstract class IndicatorsBaseTest extends MetamacBaseTests {
         sequences.add("SEQ_INDICATORS_SYSTEMS");
         sequences.add("SEQ_DIMENSIONS");
         sequences.add("SEQ_DATA_SOURCES");
-        sequences.add("SEQ_DATA_SOURCE_VARIABLES");
-        sequences.add("SEQ_INDICATOR_VERSIONS");
+        sequences.add("SEQ_DATA_SOURCES_VARIABLES");
+        sequences.add("SEQ_INDICATORS_VERSIONS");
         sequences.add("SEQ_INDICATORS");
-        sequences.add("SEQ_INDICATOR_INSTANCES");
+        sequences.add("SEQ_INDICATORS_INSTANCES");
         sequences.add("SEQ_ELEMENTS_LEVELS");
         return sequences;
     }
