fae1a04df50e9efeb10545d8b42eacbc1c11b7fa
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-02-24 14:18:49 +0000

    issue INDISTAC-86: Modificar arquitectura ServiceFacade, Service. Se deja sólo un Facade
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@33655 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ indicators-core/src/test/java/es/gobcan/istac/indicators/core/serviceapi/IndicatorsServiceFacadeTest.java
@@ -0,0 +1,16 @@
+package es.gobcan.istac.indicators.core.serviceapi;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * No tests
+ */
+public class IndicatorsServiceFacadeTest {
+    
+    @Test
+    @Ignore
+    public void noTest() {
+        // Test are in IndicatorsServiceFacadeIndicatorsSystemsTest and IndicatorsServiceFacadeIndicatorsTest
+    }
+}
