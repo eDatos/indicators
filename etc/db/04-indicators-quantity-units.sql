f124f934ab0084d2e14729cac39750d87478f624
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-03-02 15:00:51 +0000

    Actualizadas secuencias en el script de inserción de quantity units
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@33944 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- etc/db/04-indicators-quantity-units.sql
+++ etc/db/04-indicators-quantity-units.sql
@@ -7,3 +7,12 @@ INSERT INTO TBL_INTERNATIONAL_STRINGS (ID, UUID, VERSION) values (2, '2', 1);
 INSERT INTO TBL_LOCALISED_STRINGS (ID, UUID, LABEL, LOCALE, INTERNATIONAL_STRING_FK, VERSION) values (3, '3', 'meters', 'en',  2, 1);
 INSERT INTO TBL_LOCALISED_STRINGS (ID, UUID, LABEL, LOCALE, INTERNATIONAL_STRING_FK, VERSION) values (4, '4', 'metros', 'es',  2, 1);
 INSERT INTO LIS_QUANTITIES_UNITS (ID, UUID, SYMBOL, SYMBOL_POSITION, TEXT_FK, VERSION) values (2, '2', 'm', 'END', 2, 1);
+
+-- Update sequences
+alter sequence SEQ_I18NSTRS increment by 2;
+select SEQ_I18NSTRS.nextval from dual;
+alter sequence SEQ_I18NSTRS increment by 1; 
+
+alter sequence SEQ_L10NSTRS increment by 4;
+select SEQ_L10NSTRS.nextval from dual;
+alter sequence SEQ_L10NSTRS increment by 1;
