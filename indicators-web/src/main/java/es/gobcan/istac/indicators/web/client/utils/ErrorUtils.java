8a7a2e890f860c54840cfd3b9c0d1b901626b2ae
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-03-05 12:32:55 +0000

    ErrorUtils extiende de CommonErrorUtils
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@33957 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-web/src/main/java/es/gobcan/istac/indicators/web/client/utils/ErrorUtils.java
+++ indicators-web/src/main/java/es/gobcan/istac/indicators/web/client/utils/ErrorUtils.java
@@ -1,24 +1,28 @@
 package es.gobcan.istac.indicators.web.client.utils;
 
 
-import java.io.Serializable;
+import static es.gobcan.istac.indicators.web.client.IndicatorsWeb.getCoreMessages;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import org.siemac.metamac.web.common.client.utils.CommonErrorUtils;
 import org.siemac.metamac.web.common.shared.exception.MetamacWebException;
 import org.siemac.metamac.web.common.shared.exception.MetamacWebExceptionItem;
 
-import es.gobcan.istac.indicators.web.client.IndicatorsWeb;
 
-
-public class ErrorUtils {
+public class ErrorUtils extends CommonErrorUtils {
 
     public static List<String> getErrorMessages(Throwable caught, String alternativeMessage) {
         List<String> list = new ArrayList<String>();
         if (caught instanceof MetamacWebException) {
             List<MetamacWebExceptionItem> metamacExceptionItems = ((MetamacWebException) caught).getWebExceptionItems();
             for (MetamacWebExceptionItem item : metamacExceptionItems) {
-                list.add(getMessage(item));
+                if (EXCEPTION_UNKNOWN.equals(item.getCode())) {
+                    list.add(alternativeMessage);
+                } else {
+                    list.add(getMessage(item));
+                }
             }
         } else {
             list.add(alternativeMessage);
@@ -26,36 +30,14 @@ public class ErrorUtils {
         return list;
     }
 
-    public static List<String> getMessageList(String ...messages) {
-        List<String> messageList = new ArrayList<String>();
-        for (String message : messages) {
-            messageList.add(message);
-        }
-        return messageList;
-    }
-    
     private static String getMessage(MetamacWebExceptionItem item) {
         if (item != null && item.getCode() != null) {
             // GWT generate a "_" separated method when the key is separated by "."
             String code = item.getCode().replace(".", "_");
-            String message =IndicatorsWeb.getCoreMessages().getString(code);
+            String message = getCoreMessages().getString(code);
             return getMessageWithParameters(message, item.getMessageParameters());
         }
         return new String();
-    }
-    
-    private static String getMessageWithParameters(String message, Serializable[] parameters) {
-        if (parameters != null) {
-            int i = 0;
-            while (i < parameters.length) {
-                String delimiter = "{" + i + "}";
-                while(message.contains(delimiter)) {
-                    message = message.replace( delimiter, String.valueOf(parameters[i]));
-                }
-                i++;
-            }
-        }
-        return message;
     }
     
 }
