2841ac77f144017bcb6795b3c18ef181c1c6011e
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-03-13 09:16:27 +0000

    Creado paquete para constantes. Movidas expresiones regulares para variables temporales a clase de constantes para ser utilizadas también en la web
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@34134 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-core/src/main/java/es/gobcan/istac/indicators/core/IndicatorsConstants.java
+++
@@ -1,9 +0,0 @@
-package es.gobcan.istac.indicators.core;
-
-public class IndicatorsConstants {
-
-    public static final String VERSION_NUMBER_INITIAL = "1.000";
-
-    public static final String LOCALE_SPANISH         = "es";
-
-}
