4b7ab7ddd325ffcf40df196b07eef29820efcd8a
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-03-19 10:38:40 +0000

    IN PROGRESS - issue INDISTAC-166: Gestión del ciclo de vida de indicadores y sistemas de indicadores http://jira.arte-consultores.com/browse/INDISTAC-166 AskVersionWindow
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@34244 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ indicators-web/src/main/java/es/gobcan/istac/indicators/web/client/indicator/widgets/AskVersionWindow.java
@@ -0,0 +1,57 @@
+package es.gobcan.istac.indicators.web.client.indicator.widgets;
+
+import static es.gobcan.istac.indicators.web.client.IndicatorsWeb.getConstants;
+
+import org.siemac.metamac.web.common.client.MetamacWebCommon;
+import org.siemac.metamac.web.common.client.widgets.CustomWindow;
+import org.siemac.metamac.web.common.client.widgets.form.CustomDynamicForm;
+import org.siemac.metamac.web.common.client.widgets.form.fields.RequiredSelectItem;
+
+import com.smartgwt.client.types.Alignment;
+import com.smartgwt.client.widgets.form.fields.ButtonItem;
+import com.smartgwt.client.widgets.form.fields.events.HasClickHandlers;
+
+import es.gobcan.istac.indicators.core.enume.domain.VersiontTypeEnum;
+import es.gobcan.istac.indicators.web.client.utils.CommonUtils;
+
+
+public class AskVersionWindow extends CustomWindow {
+
+    private static final String FIELD_VERSION = "version-ind";
+    private static final String FIELD_SAVE = "save-ind";
+    
+    private CustomDynamicForm form;
+    
+    
+    public AskVersionWindow(String title) {
+        super(title);
+        setHeight(100);
+        setWidth(330);
+
+        RequiredSelectItem versionItem = new RequiredSelectItem(FIELD_VERSION, getConstants().indicDetailVersion());
+        versionItem.setValueMap(CommonUtils.getVersionTypeValueMap());
+        
+        ButtonItem saveItem = new ButtonItem(FIELD_SAVE, MetamacWebCommon.getConstants().actionSave());
+        saveItem.setAlign(Alignment.RIGHT);
+        
+        form = new CustomDynamicForm();
+        form.setFields(versionItem, saveItem);
+        
+        addItem(form);
+        show();
+    }
+    
+    public VersiontTypeEnum getSelectedVersion() {
+        String value = form.getValueAsString(FIELD_VERSION); 
+        return (value != null && !value.isEmpty()) ? VersiontTypeEnum.valueOf(value) : null;
+    }
+    
+    public boolean validateForm() {
+        return form.validate();
+    }
+    
+    public HasClickHandlers getSave() {
+        return form.getItem(FIELD_SAVE);
+    }
+    
+}
