5c3001a793e52c79c8749b59b7cba8dd100f3977
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-03-22 11:27:26 +0000

    IN PROGRESS - issue INDISTAC-189: Preparar cliente de web service de Gopestat http://jira.arte-consultores.com/browse/INDISTAC-189
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@34322 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-web/src/main/java/es/gobcan/istac/indicators/web/server/ws/WebservicesLocator.java
+++ indicators-web/src/main/java/es/gobcan/istac/indicators/web/server/ws/WebservicesLocator.java
@@ -2,80 +2,42 @@ package es.gobcan.istac.indicators.web.server.ws;
 
 import java.util.Map;
 
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
 import javax.xml.ws.BindingProvider;
-import javax.xml.ws.soap.SOAPBinding;
 
+import org.siemac.metamac.core.common.conf.ConfigurationService;
 import org.siemac.metamac.gopestat.internal.ws.v1_0.MetamacGopestatInternalInterfaceV10;
 import org.siemac.metamac.gopestat.internal.ws.v1_0.MetamacGopestatInternalV10;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
+public class WebservicesLocator {
 
-public class WebservicesLocator implements ServletContextListener {
+    private final String GOPESTAT_INTERNAL_ENDPOINT_ADDRESS_PROPERTY = "indicators.ws.metamac.gopestat.internal.endpoint";
+    
+    @Autowired
+    private ConfigurationService                configurationService;
 
-    private static WebservicesLocator   INSTANCE                        = new WebservicesLocator();
-
-    private static String                   GOPESTAT_INTERNAL_ENDPOINT_ADDRESS;
-
-    static {
-        GOPESTAT_INTERNAL_ENDPOINT_ADDRESS = "http://localhost:8080/gopestat-web/ws/gopestat/internal/v1.0/gopestatInternalInterface"; // TODO
-    }
-
-//    @WebServiceRef(name = "metamacGopestatInternal")
-    private MetamacGopestatInternalV10 metamacGopestatInternal;
-
-
-    public static WebservicesLocator getInstance() {
-        return INSTANCE;
-    }
-
-    // Implementantion: ServletContextListener
-    @Override
-    public void contextDestroyed(ServletContextEvent arg0) {
-        // do nothing
-    }
-
-    @Override
-    public void contextInitialized(ServletContextEvent arg0) {
-        INSTANCE = this;
-    }
-
-//    private static ThreadLocal<WSPorts> ports = new ThreadLocal<WSPorts>() {
-//        @Override
-//        protected WSPorts initialValue() {
-//            return new WSPorts();
-//        }
-//    };
+    private MetamacGopestatInternalInterfaceV10 gopestatInternalInterface;
 
     public MetamacGopestatInternalInterfaceV10 getGopestatInternalInterface() {
-//        WSPorts wsPorts = ports.get();
-//        if (wsPorts.getMetamacGopestatInternalInterface() == null) {
-//            synchronized (this) {
-//                if (wsPorts.getMetamacGopestatInternalInterface() == null) {
-                    MetamacGopestatInternalInterfaceV10 metamacGopestatInternalInterface = metamacGopestatInternal.getMetamacGopestatInternalSOAPV10();
-                    /* La configuracion solo se carga una vez por thread aunque sea dinamico */
-                    updateWSPort(metamacGopestatInternalInterface, GOPESTAT_INTERNAL_ENDPOINT_ADDRESS);
-//                    wsPorts.setMetamacGopestatInternalInterface(metamacGopestatInternalInterface);
-//                }
-//            }
-//        }
-//        return wsPorts.getMetamacGopestatInternalInterface();
-          return metamacGopestatInternalInterface;
+        if (gopestatInternalInterface == null) {
+            // TODO thread local?
+            MetamacGopestatInternalV10 metamacGopestatInternal = new MetamacGopestatInternalV10();
+            gopestatInternalInterface = metamacGopestatInternal.getMetamacGopestatInternalSOAPV10();
+            String port = configurationService.getProperties().getProperty(GOPESTAT_INTERNAL_ENDPOINT_ADDRESS_PROPERTY);
+            updateWSPort(gopestatInternalInterface, port);
+            
+        }
+        return gopestatInternalInterface;
     }
 
-    
-    private void enableMtom(Object port) {
-        BindingProvider bp = (BindingProvider) port;
-        SOAPBinding binding = (SOAPBinding) bp.getBinding();
-        binding.setMTOMEnabled(true);
-    }
-
-    private void updateWSPort(Object port, String url/*, String userName, String password*/) {
+    private void updateWSPort(Object port, String url/* , String userName, String password */) {
         BindingProvider bp = (BindingProvider) port;
         Map<String, Object> rctx = bp.getRequestContext();
         rctx.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, url);
         // TODO webservice user
-//        rctx.put(BindingProvider.USERNAME_PROPERTY, userName);
-//        rctx.put(BindingProvider.PASSWORD_PROPERTY, password);
+        // rctx.put(BindingProvider.USERNAME_PROPERTY, userName);
+        // rctx.put(BindingProvider.PASSWORD_PROPERTY, password);
     }
 }
