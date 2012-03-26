0e087f9fb9a9902b5734529627d4cca8afecdc27
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-03-26 09:26:45 +0000

    Evitar errores en ErrorUtils: - Capturar excepciones en ErrorUtils para evitar errores al hacer un lookup de una property que no existe en un fichero de mensajes - Mostrar mensaje de error personalizado si los exceptionItems están vacíos
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@34380 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-web/src/main/java/es/gobcan/istac/indicators/web/client/utils/ErrorUtils.java
+++ indicators-web/src/main/java/es/gobcan/istac/indicators/web/client/utils/ErrorUtils.java
@@ -5,6 +5,8 @@ import static es.gobcan.istac.indicators.web.client.IndicatorsWeb.getCoreMessage
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.siemac.metamac.web.common.client.utils.CommonErrorUtils;
 import org.siemac.metamac.web.common.shared.exception.MetamacWebException;
@@ -13,15 +15,28 @@ import org.siemac.metamac.web.common.shared.exception.MetamacWebExceptionItem;
 
 public class ErrorUtils extends CommonErrorUtils {
 
+    private static Logger logger = Logger.getLogger(ErrorUtils.class.getName());
+    
+    
     public static List<String> getErrorMessages(Throwable caught, String alternativeMessage) {
         List<String> list = new ArrayList<String>();
         if (caught instanceof MetamacWebException) {
             List<MetamacWebExceptionItem> metamacExceptionItems = ((MetamacWebException) caught).getWebExceptionItems();
-            for (MetamacWebExceptionItem item : metamacExceptionItems) {
-                if (EXCEPTION_UNKNOWN.equals(item.getCode())) {
-                    list.add(alternativeMessage);
-                } else {
-                    list.add(getMessage(item));
+            if (metamacExceptionItems.isEmpty()) {
+                list.add(alternativeMessage);
+            } else {
+                for (MetamacWebExceptionItem item : metamacExceptionItems) {
+                    if (EXCEPTION_UNKNOWN.equals(item.getCode())) {
+                        // If exception code is EXCEPTION_UNKNOWN, show alternative message
+                        list.add(alternativeMessage);
+                    } else {
+                        // If there is any problem getting message from code, show alternative message
+                        try {
+                            list.add(getMessage(item));
+                        } catch (Exception e) {
+                            list.add(alternativeMessage);
+                        }
+                    }
                 }
             }
         } else {
@@ -30,14 +45,22 @@ public class ErrorUtils extends CommonErrorUtils {
         return list;
     }
 
-    private static String getMessage(MetamacWebExceptionItem item) {
+    private static String getMessage(MetamacWebExceptionItem item) throws Exception {
         if (item != null && item.getCode() != null) {
             // GWT generate a "_" separated method when the key is separated by "."
             String code = item.getCode().replace(".", "_");
-            String message = getCoreMessages().getString(code);
-            return getMessageWithParameters(message, item.getMessageParameters());
+            try {
+                String message = getCoreMessages().getString(code);
+                return getMessageWithParameters(message, item.getMessageParameters());
+            } catch (Exception e) {
+                String errorMessage = "Message with code = " + code + " not found"; 
+                logger.log(Level.SEVERE, errorMessage);
+                throw new Exception(errorMessage);
+            }
         }
-        return new String();
+        String errorMessage = "Message with null code";
+        logger.log(Level.SEVERE, errorMessage);
+        throw new Exception(errorMessage);
     }
     
 }
