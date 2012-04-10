ccbbf4e30c7154a7dd77defb3b741ff330030929
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-04-10 10:34:08 +0000

    Uso de IndicatorsSystemDtoWeb en client side
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@34866 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ indicators-internal-web/src/main/java/es/gobcan/istac/indicators/web/shared/dto/IndicatorsSystemDtoWeb.java
@@ -0,0 +1,57 @@
+package es.gobcan.istac.indicators.web.shared.dto;
+
+import org.siemac.metamac.core.common.dto.serviceapi.InternationalStringDto;
+
+import es.gobcan.istac.indicators.core.dto.serviceapi.IndicatorsSystemDto;
+
+public class IndicatorsSystemDtoWeb extends IndicatorsSystemDto {
+
+    private static final long      serialVersionUID = 1L;
+
+    private String                 statisticalOperationUri;
+    private InternationalStringDto title;
+    private InternationalStringDto acronym;
+    private InternationalStringDto description;
+    private InternationalStringDto objective;
+
+    public String getStatisticalOperationUri() {
+        return statisticalOperationUri;
+    }
+
+    public void setStatisticalOperationUri(String statisticalOperationUri) {
+        this.statisticalOperationUri = statisticalOperationUri;
+    }
+
+    public InternationalStringDto getTitle() {
+        return title;
+    }
+
+    public void setTitle(InternationalStringDto title) {
+        this.title = title;
+    }
+
+    public InternationalStringDto getAcronym() {
+        return acronym;
+    }
+
+    public void setAcronym(InternationalStringDto acronym) {
+        this.acronym = acronym;
+    }
+
+    public InternationalStringDto getDescription() {
+        return description;
+    }
+
+    public void setDescription(InternationalStringDto description) {
+        this.description = description;
+    }
+
+    public InternationalStringDto getObjective() {
+        return objective;
+    }
+
+    public void setObjective(InternationalStringDto objective) {
+        this.objective = objective;
+    }
+
+}
