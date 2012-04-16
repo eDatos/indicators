daeee734e2d06d42134762998d37dcc9e922e9d6
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-04-16 07:14:26 +0000

    Nuevos campos de datasource
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@34998 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-internal-web/src/main/java/es/gobcan/istac/indicators/web/client/model/ds/DataSourceDS.java
+++ indicators-internal-web/src/main/java/es/gobcan/istac/indicators/web/client/model/ds/DataSourceDS.java
@@ -7,30 +7,36 @@ import com.smartgwt.client.data.fields.DataSourceTextField;
 
 public class DataSourceDS extends DataSource {
 
-    public static String UUID                        = "ds-uuid";
-    public static String QUERY                       = "ds-query";
-    public static String PX                          = "ds-px";
-    public static String TIME_VARIABLE               = "ds-time-var";
-    public static String TIME_VALUE                  = "ds-time-value";
-    public static String GEO_VARIABLE                = "ds-geo-var";
-    public static String GEO_VALUE                   = "ds-geo-val";
-    public static String MEASURE_VARIABLE            = "ds-meas-var";
-    public static String SOURCE_SURVEY_CODE          = "ds-sur-code";
-    public static String SOURCE_SURVEY_TITLE         = "ds-sur-title";
-    public static String SOURCE_SURVEY_ACRONYM       = "ds-sur-acro";
-    public static String SOURCE_SURVEY_URL           = "ds-sur-url";
-    public static String PUBLISHERS                  = "ds-pub";
+    public static String UUID                              = "ds-uuid";
+    public static String QUERY                             = "ds-query";
+    public static String PX                                = "ds-px";
+    public static String TIME_VARIABLE                     = "ds-time-var";
+    public static String TIME_VALUE                        = "ds-time-value";
+    public static String GEO_VARIABLE                      = "ds-geo-var";
+    public static String GEO_VALUE                         = "ds-geo-val";
+    public static String MEASURE_VARIABLE                  = "ds-meas-var";
+    public static String SOURCE_SURVEY_CODE                = "ds-sur-code";
+    public static String SOURCE_SURVEY_TITLE               = "ds-sur-title";
+    public static String SOURCE_SURVEY_ACRONYM             = "ds-sur-acro";
+    public static String SOURCE_SURVEY_URL                 = "ds-sur-url";
+    public static String PUBLISHERS                        = "ds-pub";
 
-    public static String RATE_DERIVATION_METHOD      = "rate-method";
-    public static String RATE_DERIVATION_METHOD_TYPE = "rate-method-type";
-    public static String RATE_DERIVATION_ROUNDING    = "rate-rounding";
-    public static String OTHER_VARIABLES             = "ds-other-var";
+    public static String RATE_DERIVATION_METHOD_VIEW       = "rate-method";          // Not mapped in DTO
+    public static String RATE_DERIVATION_METHOD_CALCULATED = "rate-method-cal";
+    public static String RATE_DERIVATION_METHOD_LOAD       = "rate-method-load";
 
-    public static String VARIABLE_NAME               = "var-name";
-    public static String VARIABLE_CATEGORY           = "var-cat";
-    public static String VARIABLE_DTO                = "var-dto";
+    public static String RATE_DERIVATION_METHOD_TYPE_VIEW  = "rate-method-type-view"; // Not mapped in DTO
+    public static String RATE_DERIVATION_METHOD_TYPE       = "rate-method-type";
 
-    public static String DTO                         = "ds-dto";
+    public static String RATE_DERIVATION_ROUNDING          = "rate-rounding";
+
+    public static String OTHER_VARIABLES                   = "ds-other-var";
+
+    public static String VARIABLE_NAME                     = "var-name";
+    public static String VARIABLE_CATEGORY                 = "var-cat";
+    public static String VARIABLE_DTO                      = "var-dto";
+
+    public static String DTO                               = "ds-dto";
 
     public DataSourceDS() {
         DataSourceTextField uuid = new DataSourceTextField(UUID, getConstants().dataSourceUuid());
