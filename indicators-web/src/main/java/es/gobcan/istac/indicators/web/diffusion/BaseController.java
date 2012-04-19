328f12c272e03d53abf618c9e0825c8ca5433019
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-04-19 12:47:10 +0000

    IN PROGRESS - issue INDISTAC-174: Aplicar seguridad de usuarios
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@35190 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-web/src/main/java/es/gobcan/istac/indicators/web/diffusion/BaseController.java
+++ indicators-web/src/main/java/es/gobcan/istac/indicators/web/diffusion/BaseController.java
@@ -3,11 +3,17 @@ package es.gobcan.istac.indicators.web.diffusion;
 import javax.servlet.http.HttpServletRequest;
 
 import org.fornax.cartridges.sculptor.framework.errorhandling.ServiceContext;
+import org.siemac.metamac.sso.client.MetamacPrincipal;
+import org.siemac.metamac.sso.client.MetamacPrincipalAccess;
+import org.siemac.metamac.sso.client.SsoClientConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
+
+import es.gobcan.istac.indicators.core.constants.IndicatorsConstants;
+import es.gobcan.istac.indicators.core.enume.domain.RoleEnum;
 
 public abstract class BaseController {
 
@@ -40,8 +46,13 @@ public abstract class BaseController {
         return getModelAndView(WebConstants.VIEW_NAME_ERROR_500, ex);
     }
 
-    // TODO ServiceContext
+    // TODO Ha de invocarse a API Rest. No se debe invocar al servicio, así que no habrá usuarios
     protected ServiceContext getServiceContext() {
-        return new ServiceContext("public-user", null, "indicators");
+        ServiceContext ctx = new ServiceContext("public-user", null, "indicators");
+        MetamacPrincipal metamacPrincipal = new MetamacPrincipal();
+        metamacPrincipal.setUserId(ctx.getUserId());
+        metamacPrincipal.getAccesses().add(new MetamacPrincipalAccess(RoleEnum.ADMINISTRADOR.getName(), IndicatorsConstants.SECURITY_APPLICATION_ID, null));
+        ctx.setProperty(SsoClientConstants.PRINCIPAL_ATTRIBUTE, metamacPrincipal);
+        return ctx;
     }
 }
