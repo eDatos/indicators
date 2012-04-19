72c9216fad22b38c6200dc1946d3a962537778a1
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-04-19 07:10:54 +0000

    Incluir seguridad: añadir usuario y aplicación en el context
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@35154 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-internal-web/src/main/java/es/gobcan/istac/indicators/web/server/ServiceContextHelper.java
+++ indicators-internal-web/src/main/java/es/gobcan/istac/indicators/web/server/ServiceContextHelper.java
@@ -1,6 +1,12 @@
 package es.gobcan.istac.indicators.web.server;
 
 import org.fornax.cartridges.sculptor.framework.errorhandling.ServiceContext;
+import org.siemac.metamac.sso.client.MetamacPrincipal;
+import org.siemac.metamac.sso.client.MetamacPrincipalAccess;
+import org.siemac.metamac.sso.client.SsoClientConstants;
+
+import es.gobcan.istac.indicators.core.constants.IndicatorsConstants;
+import es.gobcan.istac.indicators.core.security.RoleEnum;
 
 public class ServiceContextHelper {
 
@@ -8,8 +14,14 @@ public class ServiceContextHelper {
 
     public static ServiceContext getServiceContext() {
         if (serviceContext == null) {
-            serviceContext = new ServiceContext("user", "12345", "metamac");
-            serviceContext.setProperty("locale", "en");
+            
+            serviceContext = new ServiceContext("user", "12345", IndicatorsConstants.SECURITY_APPLICATION_ID);
+            
+            MetamacPrincipal metamacPrincipal = new MetamacPrincipal();
+            metamacPrincipal.setUserId(serviceContext.getUserId());
+            metamacPrincipal.getAccesses().add(new MetamacPrincipalAccess(RoleEnum.ADMINISTRADOR.getName(), IndicatorsConstants.SECURITY_APPLICATION_ID, null));
+            serviceContext.setProperty(SsoClientConstants.PRINCIPAL_ATTRIBUTE, metamacPrincipal);
+            
         }
         return serviceContext;
     }
