298fa3f175a2bb3cd849af67703f82ef16837df3
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-04-25 13:52:08 +0000

    Format y quitar imports innecesarios
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@35446 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-internal-web/src/main/java/es/gobcan/istac/indicators/web/server/logging/LoggingSetup.java
+++ indicators-internal-web/src/main/java/es/gobcan/istac/indicators/web/server/logging/LoggingSetup.java
@@ -1,18 +1,16 @@
 package es.gobcan.istac.indicators.web.server.logging;
 
 import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.Resource;
-import org.springframework.util.Log4jConfigurer;
 
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.joran.JoranConfigurator;
 import ch.qos.logback.core.joran.spi.JoranException;
 
-
 public class LoggingSetup {
+
     private Resource logConfigurationFile = null;
 
     public void setLogConfigurationFile(Resource logFile) {
@@ -26,14 +24,14 @@ public class LoggingSetup {
             if (logConfigurationFile == null) {
                 throw new RuntimeException("Logging configuration file has not been set \"logConfigurationFile\"");
             }
-            LoggerContext loggerContext = (LoggerContext)LoggerFactory.getILoggerFactory();
+            LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
             try {
                 JoranConfigurator configurator = new JoranConfigurator();
                 configurator.setContext(loggerContext);
                 loggerContext.reset();
                 configurator.doConfigure(logConfigurationFile.getInputStream());
             } catch (JoranException e) {
-                throw new RuntimeException("Error configuring logging system",e);
+                throw new RuntimeException("Error configuring logging system", e);
             }
         } catch (Throwable e) {
             throw new Exception(e);
