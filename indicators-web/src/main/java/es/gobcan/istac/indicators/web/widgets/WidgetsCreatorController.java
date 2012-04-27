1b96c1d4b427e1f27b8b131c41270aed5d794e3b
aherfer <aherfer@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-04-27 08:22:18 +0000

    Borrado método de prueba para la paginación de los widgets
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@35517 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-web/src/main/java/es/gobcan/istac/indicators/web/widgets/WidgetsCreatorController.java
+++ indicators-web/src/main/java/es/gobcan/istac/indicators/web/widgets/WidgetsCreatorController.java
@@ -24,40 +24,9 @@ public class WidgetsCreatorController extends BaseController {
     
     @RequestMapping(value = "/widgets/creator", method = RequestMethod.GET)
     public ModelAndView creator() throws Exception {
-
         // View
         ModelAndView modelAndView = new ModelAndView("widgets/creator");
-        //modelAndView.addObject("indicatorsSystems", indicatorsSystemsJson);
-        
         return modelAndView;
-    }
-
-    @RequestMapping(value = "/api/systems", method = RequestMethod.GET)
-    @ResponseBody
-    public ResponseEntity<PaginateResult> systems(@RequestParam("start") Integer start, @RequestParam("size") Integer size) throws Exception {
-        //Método mock para probar la páginación hasta que esté lista la api
-
-        PaginateResult paginateResult = new PaginateResult();
-        for(int i = start; i < start + size; i++){
-            System system = new System("System " + i);
-            paginateResult.results.add(system);
-        }
-        paginateResult.total = 80;
-        ResponseEntity<PaginateResult> responseEntity = new ResponseEntity<PaginateResult>(paginateResult, HttpStatus.OK);
-        return responseEntity;
-    }
-
-    private class PaginateResult {
-        public List<Object> results = new ArrayList<Object>();
-        public int total;
-    }
-
-    private class System {
-        public String name;
-
-        public System(String name){
-            this.name = name;
-        }
     }
 
 }
