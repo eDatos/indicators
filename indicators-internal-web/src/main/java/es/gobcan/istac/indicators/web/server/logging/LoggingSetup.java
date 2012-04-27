cf3d5de843e798613d4f7d4e7a94cbad6891d4aa
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-04-27 14:20:18 +0000

    in progress INDISTAC-352 Mover environment.xml a la aplicación web, en vez de Tomcat
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@35536 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-internal-web/src/main/java/es/gobcan/istac/indicators/web/server/logging/LoggingSetup.java
+++ indicators-internal-web/src/main/java/es/gobcan/istac/indicators/web/server/logging/LoggingSetup.java
@@ -1,20 +1,23 @@
 package es.gobcan.istac.indicators.web.server.logging;
 
+import java.net.URL;
+
 import javax.annotation.PostConstruct;
 
+import org.apache.commons.configuration.ConfigurationUtils;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.io.Resource;
 
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.joran.JoranConfigurator;
 import ch.qos.logback.core.joran.spi.JoranException;
 
+
 public class LoggingSetup {
+    private String logConfigurationFile = null;
 
-    private Resource logConfigurationFile = null;
-
-    public void setLogConfigurationFile(Resource logFile) {
-        this.logConfigurationFile = logFile;
+    public void setLogConfigurationFile(String logConfigurationFile) {
+        this.logConfigurationFile = logConfigurationFile;
     }
 
     @PostConstruct
@@ -24,18 +27,19 @@ public class LoggingSetup {
             if (logConfigurationFile == null) {
                 throw new RuntimeException("Logging configuration file has not been set \"logConfigurationFile\"");
             }
-            LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+            LoggerContext loggerContext = (LoggerContext)LoggerFactory.getILoggerFactory();
             try {
                 JoranConfigurator configurator = new JoranConfigurator();
                 configurator.setContext(loggerContext);
                 loggerContext.reset();
-                configurator.doConfigure(logConfigurationFile.getInputStream());
+                
+                URL url = ConfigurationUtils.locate(StringUtils.EMPTY, logConfigurationFile);
+                configurator.doConfigure(url);
             } catch (JoranException e) {
-                throw new RuntimeException("Error configuring logging system", e);
+                throw new RuntimeException("Error configuring logging system",e);
             }
         } catch (Throwable e) {
             throw new Exception(e);
         }
     }
-
 }
