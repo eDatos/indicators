dacb99bf7a26966c512a2ad2ef2e526800246314
jrodped <jrodped@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-05-09 07:30:17 +0000

    IN PROGRESS - issue INDISTAC-333: Permitir formato de respuesta JSONP para la API REST http://jira.arte-consultores.com/browse/INDISTAC-333
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@35679 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ indicators-rest-impl/src/main/java/es/gobcan/istac/indicators/rest/spring/web/servlet/filter/JsonpCallbackFilter.java
@@ -0,0 +1,63 @@
+package es.gobcan.istac.indicators.rest.spring.web.servlet.filter;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Map;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JsonpCallbackFilter implements Filter {
+
+    private static Logger log           = LoggerFactory.getLogger(JsonpCallbackFilter.class);
+    private static String CALBACK_PARAM = "_callback";
+
+    public void init(FilterConfig fConfig) throws ServletException {
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        @SuppressWarnings("unchecked")
+        Map<String, String[]> parms = httpRequest.getParameterMap();
+
+        if (parms.containsKey(CALBACK_PARAM)) {
+            if (log.isDebugEnabled()) {
+                log.debug("Wrapping response with JSONP callback '" + parms.get(CALBACK_PARAM)[0] + "'");
+            }
+
+            OutputStream out = httpResponse.getOutputStream();
+
+            GenericResponseWrapper wrapper = new GenericResponseWrapper(httpResponse);
+
+            chain.doFilter(request, wrapper);
+            
+            byte[] startCallbackFunction = new String(parms.get(CALBACK_PARAM)[0] + "(").getBytes();
+            byte[] endCallbackFunction = new String(");").getBytes();
+            wrapper.setContentLength(startCallbackFunction.length + wrapper.getData().length + endCallbackFunction.length);
+
+            out.write(startCallbackFunction);
+            out.write(wrapper.getData());
+            out.write(endCallbackFunction);
+
+            wrapper.setContentType("text/javascript;charset=UTF-8");
+
+            out.close();
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
+
+    public void destroy() {
+    }
+}
