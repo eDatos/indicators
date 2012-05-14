007e563a734070e04567e6d2a58263623dfda97f
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-05-14 10:14:57 +0000

    Compatibilidad con IE
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@35871 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-internal-web/src/main/java/es/gobcan/istac/indicators/web/client/resources/IndicatorsResources.java
+++ indicators-internal-web/src/main/java/es/gobcan/istac/indicators/web/client/resources/IndicatorsResources.java
@@ -3,59 +3,78 @@ package es.gobcan.istac.indicators.web.client.resources;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ClientBundleWithLookup;
 import com.google.gwt.resources.client.ImageResource;
+import com.google.gwt.resources.client.ImageResource.ImageOptions;
+import com.google.gwt.resources.client.ImageResource.RepeatStyle;
 
 public interface IndicatorsResources extends ClientBundleWithLookup {
 
     public static final IndicatorsResources RESOURCE = GWT.create(IndicatorsResources.class);
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("images/ok_apply.png")
     ImageResource okApply();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("images/validate_production.png")
     ImageResource validateProduction();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("images/validate_diffusion.png")
     ImageResource validateDifussion();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("images/publish.png")
     ImageResource publish();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("images/reject.png")
     ImageResource reject();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("images/archive.png")
     ImageResource archive();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("images/version.png")
     ImageResource version();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("images/reset.png")
     ImageResource reset();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("images/resultsetnext.png")
     ImageResource resultSetNext();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("images/resultsetfirst.png")
     ImageResource resultSetFirst();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("images/resultsetlast.png")
     ImageResource resultSetLast();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("images/resultsetprevious.png")
     ImageResource resultSetPrevious();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("images/resultsetnext_Disabled.png")
     ImageResource resultSetNextDisabled();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("images/resultsetfirst_Disabled.png")
     ImageResource resultSetFirstDisabled();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("images/resultsetlast_Disabled.png")
     ImageResource resultSetLastDisabled();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("images/resultsetprevious_Disabled.png")
     ImageResource resultSetPreviousDisabled();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("images/db_populate.png")
     ImageResource populateData();
 
