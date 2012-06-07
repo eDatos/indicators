1efcc714697452e82a47e5824ab79928ff371472
aherfer <aherfer@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-06-07 07:10:06 +0000

    INDISTAC-466 - El color del marco del widget será, por defecto, #0058b0
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@37287 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-web/src/main/webapp/theme/js/models/widgets/WidgetOptionsModel.js
+++ indicators-web/src/main/webapp/theme/js/models/widgets/WidgetOptionsModel.js
@@ -7,7 +7,7 @@ var WidgetOptionsModel = Backbone.Model.extend({
         title : 'Poner aquí el título del widget',
         type : 'lastData',
         width : 300,
-        borderColor : '#3478B0',
+        borderColor : '#0058b0',
         textColor : '#000000',
         systemId : '',
         indicators : [],
