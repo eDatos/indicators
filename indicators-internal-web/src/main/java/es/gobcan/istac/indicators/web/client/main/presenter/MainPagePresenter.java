bad709a4bceb52b1b678f78bbecd0fd4d0c4936d
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-06-11 11:05:15 +0000

    WaityngAsyncCallback de metamac-web-common
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@37403 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-internal-web/src/main/java/es/gobcan/istac/indicators/web/client/main/presenter/MainPagePresenter.java
+++ indicators-internal-web/src/main/java/es/gobcan/istac/indicators/web/client/main/presenter/MainPagePresenter.java
@@ -13,6 +13,7 @@ import org.siemac.metamac.web.common.client.events.SetTitleEvent;
 import org.siemac.metamac.web.common.client.events.SetTitleEvent.SetTitleHandler;
 import org.siemac.metamac.web.common.client.events.ShowMessageEvent;
 import org.siemac.metamac.web.common.client.events.ShowMessageEvent.ShowMessageHandler;
+import org.siemac.metamac.web.common.client.widgets.WaitingAsyncCallback;
 
 import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.event.shared.GwtEvent.Type;
@@ -39,7 +40,6 @@ import es.gobcan.istac.indicators.web.client.events.UpdateGeographicalGranularit
 import es.gobcan.istac.indicators.web.client.events.UpdateQuantityUnitsEvent;
 import es.gobcan.istac.indicators.web.client.main.view.handlers.MainPageUiHandlers;
 import es.gobcan.istac.indicators.web.client.utils.ErrorUtils;
-import es.gobcan.istac.indicators.web.client.widgets.WaitingAsyncCallback;
 import es.gobcan.istac.indicators.web.shared.CloseSessionAction;
 import es.gobcan.istac.indicators.web.shared.CloseSessionResult;
 import es.gobcan.istac.indicators.web.shared.GetGeographicalGranularitiesAction;
