1e39801c82f97081bbcf1024c499b85937af0403
aherfer <aherfer@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-06-15 13:41:50 +0000

    Añadido analytics
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@37780 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-web/src/main/webapp/theme/js/widgets/widget.js
+++ indicators-web/src/main/webapp/theme/js/widgets/widget.js
@@ -909,4 +909,17 @@
     window['IstacWidgetLastData'] = LastData;
     window['IstacWidget'] = Factory;
     window['IstacDataset'] = Dataset;
+
+
+
+    //Analytics
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-28460379-1']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
 })();
