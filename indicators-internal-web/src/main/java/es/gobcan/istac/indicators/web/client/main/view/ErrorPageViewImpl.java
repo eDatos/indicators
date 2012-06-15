d23e8ca852626181a018997aac2103e75749799b
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-06-15 06:40:38 +0000

    Página de error
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@37697 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-internal-web/src/main/java/es/gobcan/istac/indicators/web/client/main/view/ErrorPageViewImpl.java
+++ indicators-internal-web/src/main/java/es/gobcan/istac/indicators/web/client/main/view/ErrorPageViewImpl.java
@@ -2,9 +2,12 @@ package es.gobcan.istac.indicators.web.client.main.view;
 
 import com.google.gwt.user.client.ui.Widget;
 import com.gwtplatform.mvp.client.ViewWithUiHandlers;
-import com.smartgwt.client.widgets.Label;
+import com.smartgwt.client.types.Alignment;
+import com.smartgwt.client.widgets.HTMLFlow;
+import com.smartgwt.client.widgets.layout.HLayout;
 import com.smartgwt.client.widgets.layout.VLayout;
 
+import es.gobcan.istac.indicators.web.client.IndicatorsWeb;
 import es.gobcan.istac.indicators.web.client.main.presenter.ErrorPagePresenter;
 import es.gobcan.istac.indicators.web.client.main.view.handlers.ErrorPageUiHandlers;
 
@@ -15,7 +18,23 @@ public class ErrorPageViewImpl extends ViewWithUiHandlers<ErrorPageUiHandlers> i
     public ErrorPageViewImpl() {
         super();
         panel = new VLayout();
-        panel.addMember(new Label("Error"));
+
+        HLayout hLayout = new HLayout();
+        hLayout.setAlign(Alignment.CENTER);
+        hLayout.setMargin(20);
+
+        HTMLFlow htmlFlow = new HTMLFlow();
+        htmlFlow.setWidth(450);
+        htmlFlow.setMargin(40);
+        htmlFlow.setStyleName("exampleTextBlock");
+        StringBuffer contents = new StringBuffer();
+        contents.append("<hr>").append(IndicatorsWeb.getMessages().errorPageMessage()).append("<hr>");
+        htmlFlow.setContents(contents.toString());
+        htmlFlow.draw();
+
+        hLayout.addMember(htmlFlow);
+
+        panel.addMember(hLayout);
     }
 
     @Override
