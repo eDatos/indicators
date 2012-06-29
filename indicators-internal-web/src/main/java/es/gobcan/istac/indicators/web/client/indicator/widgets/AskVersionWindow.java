befcb69c791e4c7d98b21a53dafc7bf9b8351e14
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-06-29 07:34:28 +0000

    INDISTAC-525 Problema con ancho de modal para selección de tipo de versión
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@38293 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-internal-web/src/main/java/es/gobcan/istac/indicators/web/client/indicator/widgets/AskVersionWindow.java
+++ indicators-internal-web/src/main/java/es/gobcan/istac/indicators/web/client/indicator/widgets/AskVersionWindow.java
@@ -5,10 +5,10 @@ import static es.gobcan.istac.indicators.web.client.IndicatorsWeb.getConstants;
 import org.siemac.metamac.web.common.client.MetamacWebCommon;
 import org.siemac.metamac.web.common.client.widgets.CustomWindow;
 import org.siemac.metamac.web.common.client.widgets.form.CustomDynamicForm;
-import org.siemac.metamac.web.common.client.widgets.form.fields.RequiredSelectItem;
 
 import com.smartgwt.client.types.Alignment;
 import com.smartgwt.client.widgets.form.fields.ButtonItem;
+import com.smartgwt.client.widgets.form.fields.SelectItem;
 import com.smartgwt.client.widgets.form.fields.events.HasClickHandlers;
 
 import es.gobcan.istac.indicators.core.enume.domain.VersionTypeEnum;
@@ -26,11 +26,13 @@ public class AskVersionWindow extends CustomWindow {
         setHeight(100);
         setWidth(330);
 
-        RequiredSelectItem versionItem = new RequiredSelectItem(FIELD_VERSION, getConstants().indicDetailVersion());
+        SelectItem versionItem = new SelectItem(FIELD_VERSION, getConstants().indicDetailVersion());
+        versionItem.setRequired(true);
         versionItem.setValueMap(CommonUtils.getVersionTypeValueMap());
 
         ButtonItem saveItem = new ButtonItem(FIELD_SAVE, MetamacWebCommon.getConstants().actionSave());
-        saveItem.setAlign(Alignment.RIGHT);
+        saveItem.setColSpan(2);
+        saveItem.setAlign(Alignment.CENTER);
 
         form = new CustomDynamicForm();
         form.setFields(versionItem, saveItem);
