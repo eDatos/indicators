5351149263efbf5c86ccdbe9eae24c45597a4c12
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-07-18 09:23:25 +0000

    Refactor StatisticalOperationsRestFacadeV10 por StatisticalOperationsRestInternalFacadeV10
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@38890 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-internal-web/src/main/java/es/gobcan/istac/indicators/web/server/rest/RestApiLocator.java
+++ indicators-internal-web/src/main/java/es/gobcan/istac/indicators/web/server/rest/RestApiLocator.java
@@ -5,7 +5,7 @@ import javax.annotation.PostConstruct;
 import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
 import org.apache.cxf.jaxrs.client.WebClient;
 import org.siemac.metamac.core.common.conf.ConfigurationService;
-import org.siemac.metamac.statistical.operations.rest.internal.v1_0.service.StatisticalOperationsRestFacadeV10;
+import org.siemac.metamac.statistical.operations.rest.internal.v1_0.service.StatisticalOperationsRestInternalFacadeV10;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -13,21 +13,22 @@ import org.springframework.stereotype.Component;
 public class RestApiLocator {
 
     @Autowired
-    private ConfigurationService               configurationService;
+    private ConfigurationService                       configurationService;
 
-    private StatisticalOperationsRestFacadeV10 statisticalOperationsRestFacadeV10 = null;
+    private StatisticalOperationsRestInternalFacadeV10 statisticalOperationsRestInternalFacadeV10 = null;
 
     @PostConstruct
     public void initService() throws Exception {
         String baseApi = configurationService.getProperties().getProperty(RestApiConstants.STATISTICAL_OPERATIONS_REST_INTERNAL);
-        statisticalOperationsRestFacadeV10 = JAXRSClientFactory.create(baseApi, StatisticalOperationsRestFacadeV10.class, null, true); // true to do thread safe
+        statisticalOperationsRestInternalFacadeV10 = JAXRSClientFactory.create(baseApi, StatisticalOperationsRestInternalFacadeV10.class, null, true); // true to do thread safe
     }
 
-    public StatisticalOperationsRestFacadeV10 getStatisticalOperationsRestFacadeV10() {
+    public StatisticalOperationsRestInternalFacadeV10 getStatisticalOperationsRestFacadeV10() {
         // reset thread context
-        WebClient.client(statisticalOperationsRestFacadeV10).reset();
-        WebClient.client(statisticalOperationsRestFacadeV10).accept("application/xml");
+        WebClient.client(statisticalOperationsRestInternalFacadeV10).reset();
+        WebClient.client(statisticalOperationsRestInternalFacadeV10).accept("application/xml");
 
-        return statisticalOperationsRestFacadeV10;
+        return statisticalOperationsRestInternalFacadeV10;
     }
+    
 }
