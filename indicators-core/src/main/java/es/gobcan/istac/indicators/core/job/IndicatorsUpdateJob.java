91e61e278250d7272ab156fb4279a7c0816fcfde
rcorrod <rcorrod@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-05-24 08:43:33 +0000

    Arreglado problema con el job que no tenia transacciones, ahora usa el facade
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@46109 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-core/src/main/java/es/gobcan/istac/indicators/core/job/IndicatorsUpdateJob.java
+++ indicators-core/src/main/java/es/gobcan/istac/indicators/core/job/IndicatorsUpdateJob.java
@@ -8,10 +8,16 @@ import org.quartz.JobExecutionException;
 import org.quartz.PersistJobDataAfterExecution;
 import org.siemac.metamac.core.common.exception.MetamacException;
 import org.siemac.metamac.core.common.util.ApplicationContextProvider;
+import org.siemac.metamac.sso.client.MetamacPrincipal;
+import org.siemac.metamac.sso.client.MetamacPrincipalAccess;
+import org.siemac.metamac.sso.client.SsoClientConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import es.gobcan.istac.indicators.core.constants.IndicatorsConstants;
+import es.gobcan.istac.indicators.core.enume.domain.RoleEnum;
 import es.gobcan.istac.indicators.core.serviceapi.IndicatorsDataService;
+import es.gobcan.istac.indicators.core.serviceapi.IndicatorsServiceFacade;
 
 @PersistJobDataAfterExecution
 @DisallowConcurrentExecution
@@ -19,24 +25,31 @@ public class IndicatorsUpdateJob implements Job {
 
     private static Logger         logger = LoggerFactory.getLogger(IndicatorsUpdateJob.class);
 
-    private IndicatorsDataService indicatorsDataService;
+    
+    private IndicatorsServiceFacade indicatorsServiceFacade;
 
     public IndicatorsUpdateJob() {
     }
 
-    public IndicatorsDataService getIndicatorsDataService() {
-        if (indicatorsDataService == null) {
-            indicatorsDataService = ApplicationContextProvider.getApplicationContext().getBean(IndicatorsDataService.class);
+    private IndicatorsServiceFacade getIndicatorsServiceFacade() {
+        if (indicatorsServiceFacade == null) {
+            indicatorsServiceFacade = ApplicationContextProvider.getApplicationContext().getBean(IndicatorsServiceFacade.class);
         }
-        return indicatorsDataService;
+        return indicatorsServiceFacade;
     }
+    
 
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
 
         ServiceContext serviceContext = new ServiceContext("updateJob", context.getFireInstanceId(), "metamac-core");
+        MetamacPrincipal metamacPrincipal = new MetamacPrincipal();
+        metamacPrincipal.setUserId(serviceContext.getUserId());
+        metamacPrincipal.getAccesses().add(new MetamacPrincipalAccess(RoleEnum.ADMINISTRADOR.getName(), IndicatorsConstants.SECURITY_APPLICATION_ID, null));
+        serviceContext.setProperty(SsoClientConstants.PRINCIPAL_ATTRIBUTE, metamacPrincipal);
+        
         try {
-            getIndicatorsDataService().updateIndicatorsData(serviceContext);
+            getIndicatorsServiceFacade().updateIndicatorsData(serviceContext);
         } catch (MetamacException e) {
             logger.error("Error updating indicators Data");
         }
