ead9736fb9b6ef1bd5414e9720466e2b5288a6fa
rcorrod <rcorrod@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-07-23 08:30:56 +0000

    Corregido parpadeo y redirecciones en aplicación interna
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@47988 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-internal-web/src/main/java/es/gobcan/istac/indicators/web/client/IndicatorsWeb.java
+++ indicators-internal-web/src/main/java/es/gobcan/istac/indicators/web/client/IndicatorsWeb.java
@@ -13,12 +13,15 @@ import org.siemac.metamac.web.common.client.widgets.MetamacNavBar;
 import org.siemac.metamac.web.common.client.widgets.WaitingAsyncCallback;
 import org.siemac.metamac.web.common.shared.GetNavigationBarUrlAction;
 import org.siemac.metamac.web.common.shared.GetNavigationBarUrlResult;
+import org.siemac.metamac.web.common.shared.MockCASUserAction;
+import org.siemac.metamac.web.common.shared.MockCASUserResult;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.GWT.UncaughtExceptionHandler;
 import com.google.gwt.event.shared.UmbrellaException;
 import com.google.gwt.http.client.UrlBuilder;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.gwtplatform.mvp.client.DelayedBindRegistry;
 
 import es.gobcan.istac.indicators.web.client.gin.IndicatorsWebGinjector;
@@ -34,70 +37,30 @@ import es.gobcan.istac.indicators.web.shared.ValidateTicketResult;
  */
 public class IndicatorsWeb extends MetamacEntryPoint {
 
-    private static Logger                    logger    = Logger.getLogger(IndicatorsWeb.class.getName());
+    private static final boolean             SECURITY_ENABLED = true;
+    private static Logger                    logger           = Logger.getLogger(IndicatorsWeb.class.getName());
 
     private static MetamacPrincipal          principal;
     private static IndicatorsWebMessages     messages;
     private static IndicatorsWebCoreMessages coreMessages;
     private static IndicatorsWebConstants    constants;
 
-    private IndicatorsWebGinjector           ginjector = GWT.create(IndicatorsWebGinjector.class);
+    private IndicatorsWebGinjector           ginjector        = GWT.create(IndicatorsWebGinjector.class);
 
     @Override
     public void onModuleLoad() {
         setUncaughtExceptionHandler();
-        ginjector.getDispatcher().execute(new GetNavigationBarUrlAction(), new WaitingAsyncCallback<GetNavigationBarUrlResult>() {
 
-            @Override
-            public void onWaitFailure(Throwable caught) {
-                logger.log(Level.SEVERE, "Error loading toolbar");
-                loadSecuredApplication();
-            }
-            @Override
-            public void onWaitSuccess(GetNavigationBarUrlResult result) {
-                // Load scripts for navigation bar
-                if (result.getNavigationBarUrl() != null) {
-                    MetamacNavBar.loadScripts(result.getNavigationBarUrl());
-                } else {
-                    logger.log(Level.SEVERE, "Error loading toolbar");
-                }
-                loadSecuredApplication();
-            };
-        });
+        prepareApplication(SECURITY_ENABLED);
     }
 
-    // TODO This method should be removed to use CAS authentication
-    // Application id should be the same than the one defined in org.siemac.metamac.access.control.constants.AccessControlConstants.SECURITY_APPLICATION_ID
-    // private void loadNonSecuredApplication() {
-    // ginjector.getDispatcher().execute(new MockCASUserAction("GESTOR_ACCESOS"), new WaitingAsyncCallback<MockCASUserResult>() {
-    //
-    // @Override
-    // public void onWaitFailure(Throwable caught) {
-    // logger.log(Level.SEVERE, "Error mocking CAS user");
-    // }
-    // @Override
-    // public void onWaitSuccess(MockCASUserResult result) {
-    // IndicatorsWeb.principal = result.getMetamacPrincipal();
-    // // Load edition languages
-    // ginjector.getDispatcher().execute(new GetEditionLanguagesAction(), new WaitingAsyncCallback<GetEditionLanguagesResult>() {
-    //
-    // @Override
-    // public void onWaitFailure(Throwable caught) {
-    // logger.log(Level.SEVERE, "Error loading edition languages");
-    // // If an error occurs while loading edition languages, enable SPANISH, ENGLISH and PORTUGUESE by default
-    // ApplicationEditionLanguages.setEditionLanguages(new String[]{ApplicationEditionLanguages.SPANISH, ApplicationEditionLanguages.ENGLISH, ApplicationEditionLanguages.PORTUGUESE});
-    // loadApplication();
-    // }
-    // @Override
-    // public void onWaitSuccess(GetEditionLanguagesResult result) {
-    // ApplicationEditionLanguages.setEditionLanguages(result.getLanguages());
-    // ApplicationOrganisation.setCurrentOrganisation(ApplicationOrganisationEnum.ISTAC.name());
-    // loadApplication();
-    // }
-    // });
-    // }
-    // });
-    // }
+    public void prepareApplication(boolean securityEnabled) {
+        if (securityEnabled) {
+            loadSecuredApplication();
+        } else {
+            loadNonSecuredApplication();
+        }
+    }
 
     private void loadSecuredApplication() {
         String ticketParam = Window.Location.getParameter(TICKET);
@@ -109,7 +72,9 @@ public class IndicatorsWeb extends MetamacEntryPoint {
             Window.Location.replace(url);
             return;
         }
+
         String hash = Window.Location.getHash();
+
         String ticketHash = null;
         if (hash.contains(TICKET_HASH)) {
             ticketHash = hash.substring(hash.indexOf(TICKET_HASH) + TICKET_HASH.length(), hash.length());
@@ -118,41 +83,88 @@ public class IndicatorsWeb extends MetamacEntryPoint {
         if (ticketHash == null || ticketHash.length() == 0) {
             displayLoginView();
         } else {
+            final String ticketInValidation = ticketHash;
             String serviceUrl = Window.Location.createUrlBuilder().buildString();
-            ginjector.getDispatcher().execute(new ValidateTicketAction(ticketHash, serviceUrl), new WaitingAsyncCallback<ValidateTicketResult>() {
+            ginjector.getDispatcher().execute(new ValidateTicketAction(ticketHash, serviceUrl), new AsyncCallback<ValidateTicketResult>() {
 
                 @Override
-                public void onWaitFailure(Throwable arg0) {
+                public void onFailure(Throwable arg0) {
                     logger.log(Level.SEVERE, "Error validating ticket");
                 }
                 @Override
-                public void onWaitSuccess(ValidateTicketResult result) {
-                    IndicatorsWeb.principal = result.getMetamacPrincipal();
+                public void onSuccess(ValidateTicketResult result) {
+                    setPrincipal(result.getMetamacPrincipal());
 
-                    String url = Window.Location.createUrlBuilder().setHash("").buildString();
+                    String userHash = Window.Location.getHash();
+                    userHash = userHash.replace(TICKET_HASH + ticketInValidation, "");
+
+                    String url = Window.Location.createUrlBuilder().setHash(userHash).buildString();
                     Window.Location.assign(url);
 
                     // Load edition languages
-                    ginjector.getDispatcher().execute(new GetEditionLanguagesAction(), new WaitingAsyncCallback<GetEditionLanguagesResult>() {
-
-                        @Override
-                        public void onWaitFailure(Throwable caught) {
-                            logger.log(Level.SEVERE, "Error loading edition languages");
-                            // If an error occurs while loading edition languages, enable SPANISH, ENGLISH and PORTUGUESE by default
-                            ApplicationEditionLanguages.setEditionLanguages(new String[]{ApplicationEditionLanguages.SPANISH, ApplicationEditionLanguages.ENGLISH,
-                                    ApplicationEditionLanguages.PORTUGUESE});
-                            loadApplication();
-                        }
-                        @Override
-                        public void onWaitSuccess(GetEditionLanguagesResult result) {
-                            ApplicationEditionLanguages.setEditionLanguages(result.getLanguages());
-                            ApplicationOrganisation.setCurrentOrganisation(ApplicationOrganisationEnum.ISTAC.name());
-                            loadApplication();
-                        }
-                    });
+                    loadConfiguration();
                 }
             });
         }
+    }
+
+    private void loadNonSecuredApplication() {
+        ginjector.getDispatcher().execute(new MockCASUserAction("GESTOR_ACCESOS"), new AsyncCallback<MockCASUserResult>() {
+
+            @Override
+            public void onFailure(Throwable caught) {
+                logger.log(Level.SEVERE, "Error mocking CAS user");
+            }
+            @Override
+            public void onSuccess(MockCASUserResult result) {
+                setPrincipal(result.getMetamacPrincipal());
+
+                // Load edition languages
+                loadConfiguration();
+            }
+        });
+    }
+
+    private void loadConfiguration() {
+        // Load edition languages
+        ginjector.getDispatcher().execute(new GetEditionLanguagesAction(), new AsyncCallback<GetEditionLanguagesResult>() {
+
+            @Override
+            public void onFailure(Throwable caught) {
+                logger.log(Level.SEVERE, "Error loading edition languages");
+                // If an error occurs while loading edition languages, enable SPANISH, ENGLISH and PORTUGUESE by default
+                ApplicationEditionLanguages.setEditionLanguages(new String[]{ApplicationEditionLanguages.SPANISH, ApplicationEditionLanguages.ENGLISH, ApplicationEditionLanguages.PORTUGUESE});
+                loadNavbar();
+            }
+            @Override
+            public void onSuccess(GetEditionLanguagesResult result) {
+                ApplicationEditionLanguages.setEditionLanguages(result.getLanguages());
+                ApplicationOrganisation.setCurrentOrganisation(ApplicationOrganisationEnum.ISTAC.name());
+                loadNavbar();
+            }
+        });
+    }
+
+    private void loadNavbar() {
+        ginjector.getDispatcher().execute(new GetNavigationBarUrlAction(), new AsyncCallback<GetNavigationBarUrlResult>() {
+
+            @Override
+            public void onFailure(Throwable caught) {
+                logger.log(Level.SEVERE, "Error loading toolbar");
+                loadApplication();
+            }
+
+            @Override
+            public void onSuccess(GetNavigationBarUrlResult result) {
+                // Load scripts for navigation bar
+                if (result.getNavigationBarUrl() != null) {
+                    MetamacNavBar.loadScripts(result.getNavigationBarUrl());
+                } else {
+                    logger.log(Level.SEVERE, "Error loading toolbar");
+                }
+                loadApplication();
+            };
+        });
     }
 
     private void loadApplication() {
@@ -162,6 +174,11 @@ public class IndicatorsWeb extends MetamacEntryPoint {
         ginjector.getPlaceManager().revealCurrentPlace();
     }
 
+    protected void setPrincipal(MetamacPrincipal principal) {
+        IndicatorsWeb.principal = principal;
+    }
+
+    @Override
     public void setUncaughtExceptionHandler() {
         GWT.setUncaughtExceptionHandler(new UncaughtExceptionHandler() {
 
@@ -173,6 +190,7 @@ public class IndicatorsWeb extends MetamacEntryPoint {
         });
     }
 
+    @Override
     public Throwable unwrap(Throwable e) {
         if (e instanceof UmbrellaException) {
             UmbrellaException ue = (UmbrellaException) e;
