2b0a063fac186483b5869cc56c4afc5169971f7d
rcorrod <rcorrod@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-07-23 07:53:03 +0000

    Borrando atoms
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@47984 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-web/src/main/java/es/gobcan/istac/indicators/web/diffusion/WebConstants.java
+++ indicators-web/src/main/java/es/gobcan/istac/indicators/web/diffusion/WebConstants.java
@@ -19,15 +19,10 @@ public interface WebConstants {
     public String LOCALE_ES = "es";
     public String LOCALE_EN = "en";
 
-    // Atoms DIR
-    public String ATOMS_DIR = "atoms";
-
     // Configuration
     public String DATA_URL_PROPERTY = "environment.indicators.data";
     public String METAMAC_STATISTICAL_OPERATIONS_EXTERNAL_ENDPOINT_ADDRESS_PROPERTY = "indicators.ws.metamac.statistical.operations.external.endpoint";
     public String JAXI_URL_PROPERTY = "indicators.jaxi.url";
-    public String ATOMS_TIME_TO_LIVE_MINS_PROPERTY = "indicators.atoms.timetolive.minutes";
-    public String ATOMS_NUM_ENTRIES_PROPERTY = "indicators.atoms.entries.num";
     public String WIDGETS_TYPE_LIST_URL_PROPERTY = "indicators.widgets.typelist.url";
     public String WIDGETS_QUERY_TOOLS_URL_PROPERTY = "indicators.querytools.url";
     public String WIDGETS_SPARKLINE_MAX = "indicators.widgets.sparkline.max";
--- indicators-web/src/main/java/es/gobcan/istac/indicators/web/diffusion/indicatorssystems/IndicatorsSystemsController.java
+++ indicators-web/src/main/java/es/gobcan/istac/indicators/web/diffusion/indicatorssystems/IndicatorsSystemsController.java
@@ -1,19 +1,5 @@
 package es.gobcan.istac.indicators.web.diffusion.indicatorssystems;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.DataInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.OutputStreamWriter;
-import java.util.Locale;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.apache.commons.lang.StringUtils;
 import org.siemac.metamac.core.common.conf.ConfigurationService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,22 +8,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import es.gobcan.istac.indicators.web.diffusion.BaseController;
 import es.gobcan.istac.indicators.web.diffusion.WebConstants;
-import es.gobcan.istac.indicators.web.diffusion.atom.AtomGenerator;
 
 @Controller
 public class IndicatorsSystemsController extends BaseController {
 
     @Autowired
     private ConfigurationService configurationService;
-
-    @Autowired
-    private AtomGenerator        atomGenerator;
 
     // TODO Esta página no se va mostrar. Si se muestra, implementar la paginación
     @RequestMapping(value = "/indicatorsSystems", method = RequestMethod.GET)
@@ -63,46 +44,6 @@ public class IndicatorsSystemsController extends BaseController {
         modelAndView.addObject("jaxiUrlBase", jaxiUrlBase);
 
         return modelAndView;
-    }
-
-    //@RequestMapping(value = "/indicatorsSystems/{code}/atom.xml", method = RequestMethod.GET)
-    public void indicatorsSystemAtom(UriComponentsBuilder uriComponentsBuilder, @PathVariable("code") String code, 
-                                                                                @RequestParam(required = false, value = "language",defaultValue="es")String language, 
-                                                                                HttpServletResponse response) throws Exception {
-        String baseUrl = uriComponentsBuilder.build().toUriString();
-        String feedUrl = uriComponentsBuilder.build().toUriString() + "/indicatorsSystems/"+code+"/atom.xml?language="+language;
-        String systemUrl = uriComponentsBuilder.build().toUriString() + "/indicatorsSystems/"+code+"?language="+language;
-        Locale locale = new Locale(language);
-        String atomPath = atomGenerator.getIndicatorsSystemAtomFilePath(getServiceContext(), locale, baseUrl, systemUrl, feedUrl, code);
-        writeAtomFileInResponse(response, atomPath, code);
-    }
-
-    private void writeAtomFileInResponse(HttpServletResponse response, String filePath, String name) throws Exception {
-
-        File file = new File(filePath);
-        int length = 0;
-        ServletOutputStream op = response.getOutputStream();
-        DataInputStream in = null;
-        String mimetype = "application/atom+xml";
-
-        response.setContentType(mimetype);
-        response.setContentLength((int) file.length());
-        response.setHeader("Content-Disposition", "attachment; filename=\"" + name+".xml" + "\"");
-        try {
-            byte[] bbuf = new byte[1000];
-            in = new DataInputStream(new FileInputStream(file));
-            while ((in != null) && ((length = in.read(bbuf)) != -1)) {
-                op.write(bbuf, 0, length);
-            }
-        } finally {
-            if (in != null) {
-                in.close();
-            }
-            if (op != null) {
-                op.flush();
-                op.close();
-            }
-        }
     }
 
 }
