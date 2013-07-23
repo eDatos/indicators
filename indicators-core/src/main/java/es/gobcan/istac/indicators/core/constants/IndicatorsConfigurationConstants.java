f3ccbfd519475bd201079264ee8d090a41f5cb1c
rcorrod <rcorrod@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-07-23 12:46:01 +0000

    Quitando properties de corecommon por imcpompatibilidad de versiones
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@48012 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-core/src/main/java/es/gobcan/istac/indicators/core/constants/IndicatorsConfigurationConstants.java
+++ indicators-core/src/main/java/es/gobcan/istac/indicators/core/constants/IndicatorsConfigurationConstants.java
@@ -47,4 +47,10 @@ public class IndicatorsConfigurationConstants extends ConfigurationConstants {
 
     public static final String ENDPOINT_STATISTICAL_OPERATIONS_INTERNAL_API = "metamac.statistical_operations.rest.internal";
     public static final String ENDPOINT_STATISTICAL_OPERATIONS_EXTERNAL_API = "metamac.statistical_operations.rest.external";
+
+    // SECURITY
+    public static final String SECURITY_CAS_SERVICE_LOGIN_URL               = "metamac.security.cas_service_login_url";
+    public static final String SECURITY_CAS_SERVICE_LOGOUT_URL              = "metamac.security.cas_service_logout_url";
+    public static final String SECURITY_CAS_SERVER_URL_PREFIX               = "metamac.security.cas_server_url_prefix";
+    public static final String SECURITY_TOLERANCE                           = "metamac.security.tolerance";
 }
