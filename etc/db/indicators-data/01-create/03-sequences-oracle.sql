17c30069267235f42a78130fea8e8e3ac8191003
rcorrod <rcorrod@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-11-18 11:01:51 +0000

    creacion de dataset repository
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@50034 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ etc/db/indicators-data/01-create/03-sequences-oracle.sql
@@ -0,0 +1,6 @@
+create sequence SEQ_DATASETS;
+create sequence SEQ_DASET_DIMS;
+create sequence SEQ_ATTRIBUTES;
+create sequence SEQ_ATTR_DIMS;
+create sequence SEQ_I18NSTRS;
+create sequence SEQ_L10NSTRS;
