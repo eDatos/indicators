f71460c3d94d447cfc767da6b7b3b82b78268d32
rcorrod <rcorrod@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-11-25 08:49:15 +0000

    Actualizado a rest-common 1.3.0
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@50096 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-rest-impl/src/main/java/es/gobcan/istac/indicators/rest/mapper/IndicatorInstancesRest2DoMapperImpl.java
+++ indicators-rest-impl/src/main/java/es/gobcan/istac/indicators/rest/mapper/IndicatorInstancesRest2DoMapperImpl.java
@@ -1,24 +1,26 @@
 package es.gobcan.istac.indicators.rest.mapper;
 
-import es.gobcan.istac.indicators.core.domain.IndicatorInstance;
-import es.gobcan.istac.indicators.core.domain.IndicatorInstanceProperties;
-import es.gobcan.istac.indicators.core.domain.IndicatorVersionProperties;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.core.Response;
+
 import org.fornax.cartridges.sculptor.framework.domain.LeafProperty;
 import org.fornax.cartridges.sculptor.framework.domain.Property;
 import org.siemac.metamac.core.common.constants.CoreCommonConstants;
 import org.siemac.metamac.rest.common.query.domain.MetamacRestOrder;
 import org.siemac.metamac.rest.common.query.domain.MetamacRestQueryPropertyRestriction;
-import org.siemac.metamac.rest.common.query.domain.SculptorPropertyCriteria;
 import org.siemac.metamac.rest.exception.RestCommonServiceExceptionType;
 import org.siemac.metamac.rest.exception.RestException;
 import org.siemac.metamac.rest.exception.utils.RestExceptionUtils;
 import org.siemac.metamac.rest.search.criteria.SculptorCriteria;
+import org.siemac.metamac.rest.search.criteria.SculptorPropertyCriteria;
 import org.siemac.metamac.rest.search.criteria.mapper.RestCriteria2SculptorCriteria;
 import org.springframework.stereotype.Component;
 
-import javax.ws.rs.core.Response;
-import java.util.ArrayList;
-import java.util.List;
+import es.gobcan.istac.indicators.core.domain.IndicatorInstance;
+import es.gobcan.istac.indicators.core.domain.IndicatorInstanceProperties;
+import es.gobcan.istac.indicators.core.domain.IndicatorVersionProperties;
 
 @Component
 public class IndicatorInstancesRest2DoMapperImpl implements IndicatorInstancesRest2DoMapper {
@@ -26,10 +28,8 @@ public class IndicatorInstancesRest2DoMapperImpl implements IndicatorInstancesRe
     private RestCriteria2SculptorCriteria<IndicatorInstance> parser;
 
     public IndicatorInstancesRest2DoMapperImpl() {
-        parser = new RestCriteria2SculptorCriteria<IndicatorInstance>(
-                IndicatorInstance.class,
-                IndicatorInstancesPropertyOrder.class,
-                IndicatorInstancesPropertyRestriction.class, new IndicatorInstancesCriteriaCallback());
+        parser = new RestCriteria2SculptorCriteria<IndicatorInstance>(IndicatorInstance.class, IndicatorInstancesPropertyOrder.class, IndicatorInstancesPropertyRestriction.class,
+                new IndicatorInstancesCriteriaCallback());
     }
 
     private String integerParam2String(Integer parameter) {
@@ -62,17 +62,17 @@ public class IndicatorInstancesRest2DoMapperImpl implements IndicatorInstancesRe
             String value = propertyRestriction.getValue();
             switch (propertyNameCriteria) {
 
-                case id : {
-                    if(propertyRestriction.getValue() != null) {
-                        return new SculptorPropertyCriteria(IndicatorInstanceProperties.code(), propertyRestriction.getValue());
-                    } else if(propertyRestriction.getValueList() != null) {
+                case id: {
+                    if (propertyRestriction.getValue() != null) {
+                        return new SculptorPropertyCriteria(IndicatorInstanceProperties.code(), propertyRestriction.getValue(), propertyRestriction.getOperationType());
+                    } else if (propertyRestriction.getValueList() != null) {
                         List<Object> valueList = new ArrayList<Object>(propertyRestriction.getValueList());
-                        return new SculptorPropertyCriteria(IndicatorInstanceProperties.code(), valueList);
+                        return new SculptorPropertyCriteria(IndicatorInstanceProperties.code(), valueList, propertyRestriction.getOperationType());
                     }
                 }
 
                 case geographicalValue: {
-                    return new SculptorPropertyCriteria(IndicatorInstanceProperties.lastValuesCache().geographicalValue().code(), value);
+                    return new SculptorPropertyCriteria(IndicatorInstanceProperties.lastValuesCache().geographicalValue().code(), value, propertyRestriction.getOperationType());
                 }
 
             }
@@ -84,9 +84,7 @@ public class IndicatorInstancesRest2DoMapperImpl implements IndicatorInstancesRe
             IndicatorInstancesPropertyOrder propertyNameCriteria = IndicatorInstancesPropertyOrder.valueOf(order.getPropertyName());
             switch (propertyNameCriteria) {
                 case update: {
-                    return new LeafProperty<IndicatorInstance>(IndicatorInstanceProperties.lastUpdated().getName(),
-                            CoreCommonConstants.CRITERIA_DATETIME_COLUMN_DATETIME,
-                            true,
+                    return new LeafProperty<IndicatorInstance>(IndicatorInstanceProperties.lastUpdated().getName(), CoreCommonConstants.CRITERIA_DATETIME_COLUMN_DATETIME, true,
                             IndicatorInstance.class);
                 }
                 case id: {
--- indicators-rest-impl/src/main/java/es/gobcan/istac/indicators/rest/mapper/IndicatorsRest2DoMapperImpl.java
+++ indicators-rest-impl/src/main/java/es/gobcan/istac/indicators/rest/mapper/IndicatorsRest2DoMapperImpl.java
@@ -1,23 +1,25 @@
 package es.gobcan.istac.indicators.rest.mapper;
 
-import es.gobcan.istac.indicators.core.domain.IndicatorVersion;
-import es.gobcan.istac.indicators.core.domain.IndicatorVersionProperties;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.core.Response;
+
 import org.fornax.cartridges.sculptor.framework.domain.LeafProperty;
 import org.fornax.cartridges.sculptor.framework.domain.Property;
 import org.siemac.metamac.core.common.constants.CoreCommonConstants;
 import org.siemac.metamac.rest.common.query.domain.MetamacRestOrder;
 import org.siemac.metamac.rest.common.query.domain.MetamacRestQueryPropertyRestriction;
-import org.siemac.metamac.rest.common.query.domain.SculptorPropertyCriteria;
 import org.siemac.metamac.rest.exception.RestCommonServiceExceptionType;
 import org.siemac.metamac.rest.exception.RestException;
 import org.siemac.metamac.rest.exception.utils.RestExceptionUtils;
 import org.siemac.metamac.rest.search.criteria.SculptorCriteria;
+import org.siemac.metamac.rest.search.criteria.SculptorPropertyCriteria;
 import org.siemac.metamac.rest.search.criteria.mapper.RestCriteria2SculptorCriteria;
 import org.springframework.stereotype.Component;
 
-import javax.ws.rs.core.Response;
-import java.util.ArrayList;
-import java.util.List;
+import es.gobcan.istac.indicators.core.domain.IndicatorVersion;
+import es.gobcan.istac.indicators.core.domain.IndicatorVersionProperties;
 
 @Component
 public class IndicatorsRest2DoMapperImpl implements IndicatorsRest2DoMapper {
@@ -25,10 +27,7 @@ public class IndicatorsRest2DoMapperImpl implements IndicatorsRest2DoMapper {
     private RestCriteria2SculptorCriteria<IndicatorVersion> parser;
 
     public IndicatorsRest2DoMapperImpl() {
-        parser = new RestCriteria2SculptorCriteria<IndicatorVersion>(
-                IndicatorVersion.class,
-                IndicatorsPropertyOrder.class,
-                IndicatorsPropertyRestriction.class, new IndicatorsCriteriaCallback());
+        parser = new RestCriteria2SculptorCriteria<IndicatorVersion>(IndicatorVersion.class, IndicatorsPropertyOrder.class, IndicatorsPropertyRestriction.class, new IndicatorsCriteriaCallback());
     }
 
     private String integerParam2String(Integer parameter) {
@@ -48,7 +47,6 @@ public class IndicatorsRest2DoMapperImpl implements IndicatorsRest2DoMapper {
         geographicalValue, subjectCode, id
     }
 
-
     private class IndicatorsCriteriaCallback implements RestCriteria2SculptorCriteria.CriteriaCallback {
 
         private RestException createInvalidParameterException(String parameter) throws RestException {
@@ -62,19 +60,19 @@ public class IndicatorsRest2DoMapperImpl implements IndicatorsRest2DoMapper {
             String value = propertyRestriction.getValue();
             switch (propertyNameCriteria) {
                 case subjectCode: {
-                    return new SculptorPropertyCriteria(IndicatorVersionProperties.subjectCode(), value);
+                    return new SculptorPropertyCriteria(IndicatorVersionProperties.subjectCode(), value, propertyRestriction.getOperationType());
                 }
-                case id : {
-                    if(propertyRestriction.getValue() != null) {
-                        return new SculptorPropertyCriteria(IndicatorVersionProperties.indicator().code(), propertyRestriction.getValue());
-                    } else if(propertyRestriction.getValueList() != null) {
+                case id: {
+                    if (propertyRestriction.getValue() != null) {
+                        return new SculptorPropertyCriteria(IndicatorVersionProperties.indicator().code(), propertyRestriction.getValue(), propertyRestriction.getOperationType());
+                    } else if (propertyRestriction.getValueList() != null) {
                         List<Object> valueList = new ArrayList<Object>(propertyRestriction.getValueList());
-                        return new SculptorPropertyCriteria(IndicatorVersionProperties.indicator().code(), valueList);
+                        return new SculptorPropertyCriteria(IndicatorVersionProperties.indicator().code(), valueList, propertyRestriction.getOperationType());
                     }
                 }
 
                 case geographicalValue: {
-                    return new SculptorPropertyCriteria(IndicatorVersionProperties.lastValuesCache().geographicalValue().code(), value);
+                    return new SculptorPropertyCriteria(IndicatorVersionProperties.lastValuesCache().geographicalValue().code(), value, propertyRestriction.getOperationType());
                 }
             }
             throw createInvalidParameterException("q");
@@ -85,10 +83,7 @@ public class IndicatorsRest2DoMapperImpl implements IndicatorsRest2DoMapper {
             IndicatorsPropertyOrder propertyNameCriteria = IndicatorsPropertyOrder.valueOf(order.getPropertyName());
             switch (propertyNameCriteria) {
                 case update: {
-                    return new LeafProperty<IndicatorVersion>(IndicatorVersionProperties.lastUpdated().getName(),
-                            CoreCommonConstants.CRITERIA_DATETIME_COLUMN_DATETIME,
-                            true,
-                            IndicatorVersion.class);
+                    return new LeafProperty<IndicatorVersion>(IndicatorVersionProperties.lastUpdated().getName(), CoreCommonConstants.CRITERIA_DATETIME_COLUMN_DATETIME, true, IndicatorVersion.class);
                 }
                 case id: {
                     return IndicatorVersionProperties.id();
