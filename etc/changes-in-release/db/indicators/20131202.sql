72d79801b079a545dec5110e081b1aa9b6f905a3
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-12-12 09:39:12 +0000

    METAMAC-1976 - Posibilidad de setear en la WEB los datos por defecto de data >> indicators
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@50295 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- etc/changes-in-release/db/indicators/20131202.sql
+++ etc/changes-in-release/db/indicators/20131202.sql
@@ -1,7 +1,7 @@
 -- ---------------------------------------------------------------------------------------------------------------
 -- INDISTAC-778 - [BBDD] Nombre semántico para las tablas generadas y acceso sólo a determinados usuarios
 -- ---------------------------------------------------------------------------------------------------------------
--- The role has to be same specified in DATA
+-- The role has to be the same specified in DATA
 CREATE ROLE <FILL_ME_WITH_ROLE_NAME>;
 
 ALTER TABLE TB_INDICATORS ADD VIEW_CODE VARCHAR2(30 CHAR) NOT NULL;
