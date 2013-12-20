cd95152745211766ae3b1eb852b41718ab46f66d
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-12-20 07:33:02 +0000

    METAMAC-2013 - [DATA] Evitar que se accedan a las properties del DATA saltándose el ConfigurationService
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@50435 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-internal-web/src/main/java/es/gobcan/istac/indicators/web/shared/GetEditionLanguages.java
+++ indicators-internal-web/src/main/java/es/gobcan/istac/indicators/web/shared/GetEditionLanguages.java
@@ -1,5 +1,7 @@
 package es.gobcan.istac.indicators.web.shared;
 
+import java.util.List;
+
 import com.gwtplatform.dispatch.annotation.GenDispatch;
 import com.gwtplatform.dispatch.annotation.Out;
 
