78128c88928f43d7ccc5d4a2c3e3e238dcd51691
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-01-31 08:24:53 +0000

    INDISTAC-810 - En AbstractRestController controlar los not found de todos los recursos y devolver 404 http://jira.arte-consultores.com/browse/INDISTAC-810
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@50961 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-rest-impl/src/main/java/es/gobcan/istac/indicators/rest/controller/AbstractRestController.java
+++ indicators-rest-impl/src/main/java/es/gobcan/istac/indicators/rest/controller/AbstractRestController.java
@@ -21,18 +21,22 @@ public abstract class AbstractRestController {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorType> handleException(Exception ex, HttpServletRequest request) {
         logger.error(ex.getMessage(), ex);
-        
+
         HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
         String message = HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase();
         if (RestRuntimeException.class.isInstance(ex)) {
-            RestRuntimeException restRuntimeException = (RestRuntimeException)ex;
+            RestRuntimeException restRuntimeException = (RestRuntimeException) ex;
             httpStatus = restRuntimeException.getHttpStatus();
             message = ex.getMessage();
         } else if (MetamacException.class.isInstance(ex)) {
-            MetamacException metamacException = (MetamacException)ex;
+            MetamacException metamacException = (MetamacException) ex;
             MetamacExceptionItem metamacExceptionItem = metamacException.getExceptionItems().get(0); // TODO Contemplate all parameters?
             message = metamacExceptionItem.getMessage();
-            if (ServiceExceptionType.INDICATORS_SYSTEM_NOT_FOUND_WITH_CODE.getCode().equals(metamacExceptionItem.getCode())) { // TODO Check all excepctions
+            if (ServiceExceptionType.INDICATORS_SYSTEM_NOT_FOUND_WITH_CODE.getCode().equals(metamacExceptionItem.getCode())
+                    || ServiceExceptionType.INDICATOR_INSTANCE_NOT_FOUND_WITH_CODE.getCode().equals(metamacExceptionItem.getCode())
+                    || ServiceExceptionType.INDICATOR_NOT_FOUND_WITH_CODE.getCode().equals(metamacExceptionItem.getCode())
+                    || ServiceExceptionType.GEOGRAPHICAL_VALUE_NOT_FOUND_WITH_CODE.getCode().equals(metamacExceptionItem.getCode())
+                    || ServiceExceptionType.GEOGRAPHICAL_GRANULARITY_NOT_FOUND_WITH_CODE.getCode().equals(metamacExceptionItem.getCode())) {
                 httpStatus = HttpStatus.NOT_FOUND;
             } else {
                 httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
@@ -40,7 +44,7 @@ public abstract class AbstractRestController {
         }
 
         ErrorType errorType = new ErrorType();
-        errorType.setCode(""+httpStatus.value());
+        errorType.setCode("" + httpStatus.value());
         errorType.setMessage(message);
         ResponseEntity<ErrorType> responseEntity = new ResponseEntity<ErrorType>(errorType, httpStatus);
         return responseEntity;
