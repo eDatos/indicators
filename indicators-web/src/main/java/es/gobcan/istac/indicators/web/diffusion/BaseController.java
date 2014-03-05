72436dab5e9ac8d13aaf1023e61cfeb92c9214f4
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-03-05 09:24:53 +0000

    INDISTAC-844 - [WEB] Eliminar el serviceContext del baseController de la web externa
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@51190 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-web/src/main/java/es/gobcan/istac/indicators/web/diffusion/BaseController.java
+++ indicators-web/src/main/java/es/gobcan/istac/indicators/web/diffusion/BaseController.java
@@ -2,18 +2,11 @@ package es.gobcan.istac.indicators.web.diffusion;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.fornax.cartridges.sculptor.framework.errorhandling.ServiceContext;
-import org.siemac.metamac.sso.client.MetamacPrincipal;
-import org.siemac.metamac.sso.client.MetamacPrincipalAccess;
-import org.siemac.metamac.sso.client.SsoClientConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
-
-import es.gobcan.istac.indicators.core.constants.IndicatorsConstants;
-import es.gobcan.istac.indicators.core.enume.domain.RoleEnum;
 
 public abstract class BaseController {
 
@@ -44,15 +37,5 @@ public abstract class BaseController {
     public ModelAndView handleIOException(Exception ex, HttpServletRequest request) {
         logger.error(ex.getMessage(), ex);
         return getModelAndView(WebConstants.VIEW_NAME_ERROR_500, ex);
-    }
-
-    // TODO Ha de invocarse a API Rest. No se debe invocar al servicio, así que no habrá usuarios
-    protected ServiceContext getServiceContext() {
-        ServiceContext ctx = new ServiceContext("public-user", null, "indicators");
-        MetamacPrincipal metamacPrincipal = new MetamacPrincipal();
-        metamacPrincipal.setUserId(ctx.getUserId());
-        metamacPrincipal.getAccesses().add(new MetamacPrincipalAccess(RoleEnum.ADMINISTRADOR.getName(), IndicatorsConstants.SECURITY_APPLICATION_ID, null));
-        ctx.setProperty(SsoClientConstants.PRINCIPAL_ATTRIBUTE, metamacPrincipal);
-        return ctx;
     }
 }
