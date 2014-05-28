52461d442819c170a12015a1c081fdc3ffc9f9bb
rcorrod <rcorrod@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-05-28 13:11:55 +0000

    Subido tiempo de espera en peticiones a la api
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@52310 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-web/src/main/webapp/theme/js/app/mixins/JsonpSync.js
+++ indicators-web/src/main/webapp/theme/js/app/mixins/JsonpSync.js
@@ -2,7 +2,7 @@
     App.mixins.JsonpSync = {
         sync : function (method, model, options) {
             this.trigger('syncStart', model);
-            options.timeout = 10000;
+            options.timeout = 1000000;
             options.dataType = "jsonp";
             options.jsonp = '_callback';
 
