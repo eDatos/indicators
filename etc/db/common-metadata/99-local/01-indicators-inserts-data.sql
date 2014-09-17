fd0513c2d1c1518340dd47a67312a607cd709fbb
frodgar <frodgar@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-09-17 11:57:15 +0000

    INDISTAC-870 - Separar el WAR de la APP externa en dos WAR: APP y API >> Corregida url en script loca de bd
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@53284 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- etc/db/common-metadata/99-local/01-indicators-inserts-data.sql
+++ etc/db/common-metadata/99-local/01-indicators-inserts-data.sql
@@ -149,7 +149,7 @@ UPDATE TB_SEQUENCES SET SEQUENCE_NEXT_VALUE = SEQUENCE_NEXT_VALUE + 1 WHERE SEQU
 -- -----------------
 -- API
 -- -----------------
-insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE) values(GET_NEXT_SEQUENCE_VALUE('DATA_CONFIGURATIONS'),1,1,'indicators.rest.external','http://estadisticas.arte-consultores.com/indicators-web/api/indicators');
+insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE) values(GET_NEXT_SEQUENCE_VALUE('DATA_CONFIGURATIONS'),1,1,'indicators.rest.external','http://estadisticas.arte-consultores.com/indicators/api/indicators');
 UPDATE TB_SEQUENCES SET SEQUENCE_NEXT_VALUE = SEQUENCE_NEXT_VALUE + 1 WHERE SEQUENCE_NAME = 'DATA_CONFIGURATIONS';
 
 SET DEFINE ON
