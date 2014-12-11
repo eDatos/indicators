5e6614e87cce1cb9822c61396ed4464b15b11344
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-12-11 12:27:20 +0000

    INDISTAC-886 - [Widgets] Error de visualización del description en los widgets
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@53682 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-web/src/main/webapp/theme/js/widgets/src/Helper.js
+++ indicators-web/src/main/webapp/theme/js/widgets/src/Helper.js
@@ -1,15 +1,12 @@
-(function ($) {
+(function($) {
 
     Istac.widget.helper = {};
 
-    Istac.widget.helper.tooltip = function ($el, text, tooltipId) {
+    Istac.widget.helper.tooltip = function($el, text, tooltipId) {
         var $tooltip = $(".istact-widget-tooltip");
 
-        // Lazy init
-//        if ($tooltip.length === 0) {
-            $tooltip = $('<p class="istact-widget-tooltip' + tooltipId + '"></p>');
-            $("body").append($tooltip);
-//        }
+        $tooltip = $('<p class="istact-widget-tooltip' + tooltipId + '"></p>');
+        $("body").append($tooltip);
 
         var xOffset = 10;
         var yOffset = 20;
@@ -17,26 +14,21 @@
         if (text) {
             $tooltip.text(text);
 
-            var fnHoverIn = function (e) {
-                $tooltip
-                    .css("top", (e.pageY - xOffset) + "px")
-                    .css("left", (e.pageX + yOffset) + "px")
-                    .fadeIn("fast");
+            var fnHoverIn = function(e) {
+                $tooltip.css("top", (e.pageY - xOffset) + "px").css("left", (e.pageX + yOffset) + "px").fadeIn("fast");
             };
-            var fnHoverOut = function () {
+            var fnHoverOut = function() {
                 $tooltip.fadeOut("fast");
             };
 
             $el.hover(fnHoverIn, fnHoverOut);
-            $el.mousemove(function (e) {
-                $tooltip
-                    .css("top", (e.pageY - xOffset) + "px")
-                    .css("left", (e.pageX + yOffset) + "px");
+            $el.mousemove(function(e) {
+                $tooltip.css("top", (e.pageY - xOffset) + "px").css("left", (e.pageX + yOffset) + "px");
             });
         }
     };
 
-    Istac.widget.helper.addThousandSeparator = function (nStr) {
+    Istac.widget.helper.addThousandSeparator = function(nStr) {
         nStr += '';
         var x = nStr.split(',');
         var x1 = x[0];
@@ -48,21 +40,21 @@
         return x1 + x2;
     };
 
-    Istac.widget.helper.getHost = function (url) {
+    Istac.widget.helper.getHost = function(url) {
         var l = document.createElement("a");
         l.href = url;
         return l.host;
     };
 
-    Istac.widget.helper.absoluteUrl = function (url) {
+    Istac.widget.helper.absoluteUrl = function(url) {
         var a = document.createElement('a');
         a.href = url;
         return a.href;
     };
 
-    Istac.widget.helper.getKeys = function (hash) {
+    Istac.widget.helper.getKeys = function(hash) {
         var keys = [];
-        for (var i in hash) {
+        for ( var i in hash) {
             if (hash.hasOwnProperty(i)) {
                 keys.push(i);
             }
@@ -70,9 +62,9 @@
         return keys;
     };
 
-    Istac.widget.helper.firstKey = function (hash) {
+    Istac.widget.helper.firstKey = function(hash) {
         var key;
-        for (var i in hash) {
+        for ( var i in hash) {
             if (hash.hasOwnProperty(i)) {
                 key = i;
                 break;
@@ -81,7 +73,7 @@
         return key;
     };
 
-    Istac.widget.helper.firstValue = function (hash) {
+    Istac.widget.helper.firstValue = function(hash) {
         var key = this.firstKey(hash);
         var result;
         if (key) {
@@ -90,14 +82,14 @@
         return result;
     }
 
-    var colorHueDistributor = function (n) {
-        var result = [0];
+    var colorHueDistributor = function(n) {
+        var result = [ 0 ];
         var total = 400;
         var divisions = 3;
         var step = total / divisions;
         var offset = 0;
 
-        for(var i = 1; i < n; i ++) {
+        for (var i = 1; i < n; i++) {
             if ((i != 0) && ((i * step) % total === 0)) {
                 offset = step / 2;
             }
@@ -107,14 +99,14 @@
             }
         }
 
-        result = _.map(result, function (color) {
+        result = _.map(result, function(color) {
             return color / total;
         });
 
         return result;
     };
 
-    var hsvToRgb = function(h, s, v){
+    var hsvToRgb = function(h, s, v) {
         var r, g, b;
 
         var i = Math.floor(h * 6);
@@ -123,32 +115,44 @@
         var q = v * (1 - f * s);
         var t = v * (1 - (1 - f) * s);
 
-        switch(i % 6){
-            case 0: r = v, g = t, b = p; break;
-            case 1: r = q, g = v, b = p; break;
-            case 2: r = p, g = v, b = t; break;
-            case 3: r = p, g = q, b = v; break;
-            case 4: r = t, g = p, b = v; break;
-            case 5: r = v, g = p, b = q; break;
+        switch (i % 6) {
+            case 0:
+                r = v, g = t, b = p;
+                break;
+            case 1:
+                r = q, g = v, b = p;
+                break;
+            case 2:
+                r = p, g = v, b = t;
+                break;
+            case 3:
+                r = p, g = q, b = v;
+                break;
+            case 4:
+                r = t, g = p, b = v;
+                break;
+            case 5:
+                r = v, g = p, b = q;
+                break;
         }
 
-        return [r * 255, g * 255, b * 255];
+        return [ r * 255, g * 255, b * 255 ];
     };
 
-    var componentToHex = function (c) {
+    var componentToHex = function(c) {
         var hex = c.toString(16);
         return hex.length == 1 ? "0" + hex : hex;
     };
 
-    var rgbToHex = function (r, g, b) {
+    var rgbToHex = function(r, g, b) {
         return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
     };
 
-    Istac.widget.helper.colorPaletteGenerator = function (n) {
+    Istac.widget.helper.colorPaletteGenerator = function(n) {
         var hues = colorHueDistributor(n);
-        return _.map(hues, function (hue) {
+        return _.map(hues, function(hue) {
             var rgb = hsvToRgb(hue, 0.84, 0.84);
-            rgb = _.map(rgb, function (component) {
+            rgb = _.map(rgb, function(component) {
                 return Math.round(component);
             });
             return rgbToHex.apply(null, rgb);
