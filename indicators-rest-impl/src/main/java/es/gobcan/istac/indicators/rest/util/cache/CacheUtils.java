a2027e405de03d1c93bf0e3e8727d39aa50e9bd1
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2015-01-19 08:06:14 +0000

    SONAR - Constructor privado para clases de utilidad
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@53740 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-rest-impl/src/main/java/es/gobcan/istac/indicators/rest/util/cache/CacheUtils.java
+++ indicators-rest-impl/src/main/java/es/gobcan/istac/indicators/rest/util/cache/CacheUtils.java
@@ -1,12 +1,15 @@
 package es.gobcan.istac.indicators.rest.util.cache;
 
-import org.apache.commons.lang.StringUtils;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
+
 public class CacheUtils {
+
+    private CacheUtils() {
+    }
 
     public static String findGeographicalValuesByCodesKey(List<String> codes) {
         List<String> keyCodes = new ArrayList<String>(codes);
