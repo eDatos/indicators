4e3b0699448603125444406f588bbe506a97dc0b
frodgar <frodgar@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2015-01-26 12:20:48 +0000

    INDISTAC-885 - [Widgets] Aparece la descripción del indicador cuando se navega por el sparkline >> Corrección en visualización lateral
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@53762 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-web/src/main/webapp/theme/js/widgets/src/Templates.js
+++ indicators-web/src/main/webapp/theme/js/widgets/src/Templates.js
@@ -26,7 +26,11 @@ templates['last-data-lateral'] = template(function (Handlebars,depth0,helpers,pa
 function program1(depth0,data) {
   
   var buffer = "", stack1, foundHelper;
-  buffer += "\r\n        <div class=\"istac-widget-lateral-dataset\">\r\n            <div class=\"istac-widget-lateral-title\"><a href=\"";
+  buffer += "\r\n        <div class=\"istac-widget-lateral-dataset\">\r\n            <div data-tooltip=\"";
+  foundHelper = helpers.description;
+  if (foundHelper) { stack1 = foundHelper.call(depth0, {hash:{}}); }
+  else { stack1 = depth0.description; stack1 = typeof stack1 === functionType ? stack1() : stack1; }
+  buffer += escapeExpression(stack1) + "\" class=\"istac-widget-lateral-title\"><a href=\"";
   foundHelper = helpers.jaxiUrl;
   if (foundHelper) { stack1 = foundHelper.call(depth0, {hash:{}}); }
   else { stack1 = depth0.jaxiUrl; stack1 = typeof stack1 === functionType ? stack1() : stack1; }
@@ -52,7 +56,10 @@ function program2(depth0,data,depth1) {
 function program3(depth0,data,depth2) {
   
   var buffer = "", stack1, foundHelper;
-  buffer += "\r\n                    <div class=\"istac-widget-lateral-subtitle\">";
+  buffer += "\r\n                    <div data-tooltip=\"";
+  stack1 = depth2.description;
+  stack1 = typeof stack1 === functionType ? stack1() : stack1;
+  buffer += escapeExpression(stack1) + "\" class=\"istac-widget-lateral-subtitle\">";
   stack1 = depth2.temporalLabel;
   stack1 = typeof stack1 === functionType ? stack1() : stack1;
   buffer += escapeExpression(stack1) + " - ";
@@ -67,11 +74,14 @@ function program3(depth0,data,depth2) {
   stack1 = depth0.showSparkline;
   stack1 = helpers['if'].call(depth0, stack1, {hash:{},inverse:self.noop,fn:self.program(4, program4, data)});
   if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += " ";
+  buffer += " <span data-tooltip=\"";
+  stack1 = depth2.description;
+  stack1 = typeof stack1 === functionType ? stack1() : stack1;
+  buffer += escapeExpression(stack1) + "\">";
   foundHelper = helpers.value;
   if (foundHelper) { stack1 = foundHelper.call(depth0, {hash:{}}); }
   else { stack1 = depth0.value; stack1 = typeof stack1 === functionType ? stack1() : stack1; }
-  buffer += escapeExpression(stack1) + "</div>\r\n                ";
+  buffer += escapeExpression(stack1) + "</span></div>\r\n                ";
   return buffer;}
 function program4(depth0,data) {
   
@@ -155,7 +165,7 @@ function program5(depth0,data,depth2) {
   stack1 = depth0.showSparkline;
   stack1 = helpers['if'].call(depth0, stack1, {hash:{},inverse:self.program(8, program8, data),fn:self.program(6, program6, data)});
   if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "                   \r\n							<span data-tooltip=\"";
+  buffer += "\r\n							<span data-tooltip=\"";
   stack1 = depth2.description;
   stack1 = typeof stack1 === functionType ? stack1() : stack1;
   buffer += escapeExpression(stack1) + "\">\r\n                        		<span class=\"istac-widget-observation\">";
@@ -166,7 +176,7 @@ function program5(depth0,data,depth2) {
   foundHelper = helpers.unit;
   if (foundHelper) { stack1 = foundHelper.call(depth0, {hash:{}}); }
   else { stack1 = depth0.unit; stack1 = typeof stack1 === functionType ? stack1() : stack1; }
-  buffer += escapeExpression(stack1) + "</div>\r\n                        	</span>         \r\n                        </div>                        \r\n                    ";
+  buffer += escapeExpression(stack1) + "</div>\r\n                        	</span>\r\n                        </div>\r\n                    ";
   return buffer;}
 function program6(depth0,data) {
   
