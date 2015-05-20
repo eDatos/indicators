a843f180a4ccadeab5bac12cafd8cd1183d0fb5d
frodgar <frodgar@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2015-05-20 12:44:47 +0000

    INDISTAC-917 - [WIDGETS] El código exportar del botón de embeber no es correcto > Se modifica entonces la inicialización de la url para obtener la url absoluta
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@54105 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-web/src/main/webapp/theme/js/app/views/WidgetPreviewView.js
+++ indicators-web/src/main/webapp/theme/js/app/views/WidgetPreviewView.js
@@ -14,7 +14,7 @@
             }
             var widgetOptions = this.model.toJSON();
             widgetOptions.el = this.el;
-            widgetOptions.url = baseUrl;
+            widgetOptions.url = Istac.widget.helper.absoluteUrl(baseUrl);
             widgetOptions.apiUrl = apiUrl; 
 
             var self = this;
