d40699c98cc1e4aec15b627ea24a9bc256529859
frodgar <frodgar@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2015-10-07 08:38:52 +0000

    METAMAC-2415 - Añadir propiedad de configuración del endpoint del buscador > Añadida propiedad idxmanager.search.form.url. Modificado nombre propiedad en los scripts de sql
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@54460 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- etc/changes-from-release/4.2.2/db/common-metadata/20151006_insert-search-form-url-property.sql
+++ etc/changes-from-release/4.2.2/db/common-metadata/20151006_insert-search-form-url-property.sql
@@ -1,8 +1,9 @@
 -- -----------------------------------------------------------------------------------------
--- INDISTAC-936 - Añadir al camino de hormigas la sección de Datos abiertos
+-- METAMAC-2415 - Añadir propiedad de configuración del endpoint del buscador
+-- Véase también INDISTAC-935
 -- -----------------------------------------------------------------------------------------
 
-insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE) values(GET_NEXT_SEQUENCE_VALUE('DATA_CONFIGURATIONS'),1,1,'indicators.search.form.url','FILL_ME');
+insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE) values(GET_NEXT_SEQUENCE_VALUE('DATA_CONFIGURATIONS'),1,1,'idxmanager.search.form.url','FILL_ME');
 UPDATE TB_SEQUENCES SET SEQUENCE_NEXT_VALUE = SEQUENCE_NEXT_VALUE + 1 WHERE SEQUENCE_NAME = 'DATA_CONFIGURATIONS';
 
 commit;
