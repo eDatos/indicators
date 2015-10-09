7d83ae3e601566080a74b59dd109424a406c75aa
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2015-10-09 11:29:37 +0000

    INDISTAC-945 - Los widgets no funcionan cuando se embeben en páginas https >> Anotar las clases de FreeMarker con @Component para poder realizar el Autowired sin obtener NullPointer
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@54470 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-web/src/main/java/es/gobcan/istac/indicators/web/diffusion/view/FreeMarkerHelperView.java
+++ indicators-web/src/main/java/es/gobcan/istac/indicators/web/diffusion/view/FreeMarkerHelperView.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerView;
 
 import es.gobcan.istac.indicators.core.conf.IndicatorsConfigurationService;
@@ -13,6 +14,7 @@ import es.gobcan.istac.indicators.core.conf.IndicatorsConfigurationService;
 /**
  * FreeMarker view implementation to expose additional helpers
  */
+@Component
 public class FreeMarkerHelperView extends FreeMarkerView {
 
     @Autowired
