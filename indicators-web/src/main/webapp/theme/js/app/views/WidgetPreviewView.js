da7ec71871e53a0d47308f13718717f2737555f6
frodgar <frodgar@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2015-10-13 07:10:51 +0000

    INDISTAC-945 - Los widgets no funcionan cuando se embeben en páginas https > Eliminada mención a método eliminado
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@54477 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-web/src/main/webapp/theme/js/app/views/WidgetPreviewView.js
+++ indicators-web/src/main/webapp/theme/js/app/views/WidgetPreviewView.js
@@ -14,7 +14,7 @@
             }
             var widgetOptions = this.model.toJSON();
             widgetOptions.el = this.el;
-            widgetOptions.url = Istac.widget.helper.absoluteUrl(serverURL);
+            widgetOptions.url = serverURL;
             widgetOptions.apiUrl = apiUrl; 
 
             var self = this;
