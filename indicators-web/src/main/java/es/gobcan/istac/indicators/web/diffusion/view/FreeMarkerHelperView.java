eb34eb065288959d340fb8db1a3b7d037feda598
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2015-10-14 13:42:35 +0000

    INDISTAC-945 - Los widgets no funcionan cuando se embeben en páginas https > El cliente de widgets debe contemplar el endpoint de la API sin protocolo para que se resuelva mediante protocolo relativo.
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@54486 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-web/src/main/java/es/gobcan/istac/indicators/web/diffusion/view/FreeMarkerHelperView.java
+++ indicators-web/src/main/java/es/gobcan/istac/indicators/web/diffusion/view/FreeMarkerHelperView.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
+import org.siemac.metamac.core.common.exception.MetamacException;
 import org.siemac.metamac.core.common.util.ApplicationContextProvider;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerView;
 
@@ -15,12 +17,27 @@ import es.gobcan.istac.indicators.core.conf.IndicatorsConfigurationService;
  */
 public class FreeMarkerHelperView extends FreeMarkerView {
 
+    private static final String                   HTTP  = "http:";
+    private static final String                   HTTPS = "https:";
+
     private static IndicatorsConfigurationService configurationService;
 
     @Override
     protected void doRender(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        model.put("serverURL", getConfigurationService().retrieveIndicatorsExternalWebUrlBase());
+        String indicatorsExternalWebUrlBase = getIndicatorsExternalWebUrlBaseWithoutProtocol();
+        model.put("serverURL", indicatorsExternalWebUrlBase);
         super.doRender(model, request, response);
+    }
+
+    private String getIndicatorsExternalWebUrlBaseWithoutProtocol() throws MetamacException {
+        String indicatorsExternalWebUrlBase = getConfigurationService().retrieveIndicatorsExternalWebUrlBase();
+        if (StringUtils.startsWith(indicatorsExternalWebUrlBase, HTTP)) {
+            return StringUtils.removeStart(HTTP, indicatorsExternalWebUrlBase);
+        } else if (StringUtils.startsWith(indicatorsExternalWebUrlBase, HTTPS)) {
+            return StringUtils.removeStart(HTTPS, indicatorsExternalWebUrlBase);
+        } else {
+            return indicatorsExternalWebUrlBase;
+        }
     }
 
     private static IndicatorsConfigurationService getConfigurationService() {
