a9f89c676f6581d4d4fc109fea71e74ef345f497
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2015-10-23 06:50:46 +0000

    INDISTAC-945 - Los widgets no funcionan cuando se embeben en páginas https > En el script del common-metadata las propiedades de configuracion no deben ser relativas al protocolo
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@54511 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- etc/changes-from-release/4.2.2/db/common-metadata/20151009_insert-indicators-visualizations-url-property.sql
+++ etc/changes-from-release/4.2.2/db/common-metadata/20151009_insert-indicators-visualizations-url-property.sql
@@ -2,7 +2,7 @@
 -- INDISTAC-945 - Los widgets no funcionan cuando se embeben en páginas https
 -- -----------------------------------------------------------------------------------------
 
--- Ejemplo de valor a insertar: //estadisticas.arte-consultores.com/indicators-visualizations
+-- Ejemplo de valor a insertar: http://estadisticas.arte-consultores.com/indicators-visualizations
 insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE) values(GET_NEXT_SEQUENCE_VALUE('DATA_CONFIGURATIONS'),1,1,'indicators.web.external.url','FILL_ME');
 UPDATE TB_SEQUENCES SET SEQUENCE_NEXT_VALUE = SEQUENCE_NEXT_VALUE + 1 WHERE SEQUENCE_NAME = 'DATA_CONFIGURATIONS';
 
