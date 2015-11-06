9cda7601f81122fe318eb3fde928972a34413c41
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2015-11-06 11:32:20 +0000

    INDISTAC-956 - El metadato notes se cumplimenta automáticamente en el gestor con el valor "null"
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@54540 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- etc/changes-from-release/4.3.0/indicators-metadata/20151105_update-dirty-international-strings.sql
+++ etc/changes-from-release/4.3.0/indicators-metadata/20151105_update-dirty-international-strings.sql
@@ -6,13 +6,13 @@
 -- ..................................................................................................................................
 
 delete from TB_LOCALISED_STRINGS loc
-where loc.ID = (select loc.ID as LOCALISED_STRING_ID
+where loc.ID in (select loc.ID as LOCALISED_STRING_ID
                 from TB_INDICATORS_VERSIONS ind left join TB_INTERNATIONAL_STRINGS ins on ins.ID = ind.TITLE_FK left join TB_LOCALISED_STRINGS loc on loc.INTERNATIONAL_STRING_FK = ins.ID
                 where loc.LABEL like 'null');
 
 update TB_INDICATORS_VERSIONS indver
 set TITLE_FK = null
-where indver.TITLE_FK = (select ins.ID
+where indver.TITLE_FK in (select ins.ID
                             from TB_INTERNATIONAL_STRINGS ins
                             where ins.ID NOT IN (select loc.INTERNATIONAL_STRING_FK
                                                 from TB_LOCALISED_STRINGS loc));
@@ -22,13 +22,13 @@ where indver.TITLE_FK = (select ins.ID
 -- ..................................................................................................................................
 
 delete from TB_LOCALISED_STRINGS loc
-where loc.ID = (select loc.ID as LOCALISED_STRING_ID
+where loc.ID in (select loc.ID as LOCALISED_STRING_ID
                 from TB_INDICATORS_VERSIONS ind left join TB_INTERNATIONAL_STRINGS ins on ins.ID = ind.ACRONYM_FK left join TB_LOCALISED_STRINGS loc on loc.INTERNATIONAL_STRING_FK = ins.ID
                 where loc.LABEL like 'null');
 
 update TB_INDICATORS_VERSIONS indver
 set ACRONYM_FK = null
-where indver.ACRONYM_FK = (select ins.ID
+where indver.ACRONYM_FK in (select ins.ID
                             from TB_INTERNATIONAL_STRINGS ins
                             where ins.ID NOT IN (select loc.INTERNATIONAL_STRING_FK
                                                 from TB_LOCALISED_STRINGS loc));
@@ -38,13 +38,13 @@ where indver.ACRONYM_FK = (select ins.ID
 -- ..................................................................................................................................
 
 delete from TB_LOCALISED_STRINGS loc
-where loc.ID = (select loc.ID as LOCALISED_STRING_ID
+where loc.ID in (select loc.ID as LOCALISED_STRING_ID
                 from TB_INDICATORS_VERSIONS ind left join TB_INTERNATIONAL_STRINGS ins on ins.ID = ind.SUBJECT_TITLE_FK left join TB_LOCALISED_STRINGS loc on loc.INTERNATIONAL_STRING_FK = ins.ID
                 where loc.LABEL like 'null');
 
 update TB_INDICATORS_VERSIONS indver
 set SUBJECT_TITLE_FK = null
-where indver.SUBJECT_TITLE_FK = (select ins.ID
+where indver.SUBJECT_TITLE_FK in (select ins.ID
                             from TB_INTERNATIONAL_STRINGS ins
                             where ins.ID NOT IN (select loc.INTERNATIONAL_STRING_FK
                                                 from TB_LOCALISED_STRINGS loc));
@@ -54,13 +54,13 @@ where indver.SUBJECT_TITLE_FK = (select ins.ID
 -- ..................................................................................................................................
 
 delete from TB_LOCALISED_STRINGS loc
-where loc.ID = (select loc.ID as LOCALISED_STRING_ID
+where loc.ID in (select loc.ID as LOCALISED_STRING_ID
                 from TB_INDICATORS_VERSIONS ind left join TB_INTERNATIONAL_STRINGS ins on ins.ID = ind.CONCEPT_DESCRIPTION_FK left join TB_LOCALISED_STRINGS loc on loc.INTERNATIONAL_STRING_FK = ins.ID
                 where loc.LABEL like 'null');
 
 update TB_INDICATORS_VERSIONS indver
 set CONCEPT_DESCRIPTION_FK = null
-where indver.CONCEPT_DESCRIPTION_FK = (select ins.ID
+where indver.CONCEPT_DESCRIPTION_FK in (select ins.ID
                             from TB_INTERNATIONAL_STRINGS ins
                             where ins.ID NOT IN (select loc.INTERNATIONAL_STRING_FK
                                                 from TB_LOCALISED_STRINGS loc));
@@ -70,13 +70,13 @@ where indver.CONCEPT_DESCRIPTION_FK = (select ins.ID
 -- ..................................................................................................................................
 
 delete from TB_LOCALISED_STRINGS loc
-where loc.ID = (select loc.ID as LOCALISED_STRING_ID
+where loc.ID in (select loc.ID as LOCALISED_STRING_ID
                 from TB_INDICATORS_VERSIONS ind left join TB_INTERNATIONAL_STRINGS ins on ins.ID = ind.COMMENTS_FK left join TB_LOCALISED_STRINGS loc on loc.INTERNATIONAL_STRING_FK = ins.ID
                 where loc.LABEL like 'null');
 
 update TB_INDICATORS_VERSIONS indver
 set COMMENTS_FK = null
-where indver.COMMENTS_FK = (select ins.ID
+where indver.COMMENTS_FK in (select ins.ID
                             from TB_INTERNATIONAL_STRINGS ins
                             where ins.ID NOT IN (select loc.INTERNATIONAL_STRING_FK
                                                 from TB_LOCALISED_STRINGS loc));
@@ -86,13 +86,13 @@ where indver.COMMENTS_FK = (select ins.ID
 -- ..................................................................................................................................
 
 delete from TB_LOCALISED_STRINGS loc
-where loc.ID = (select loc.ID as LOCALISED_STRING_ID
+where loc.ID in (select loc.ID as LOCALISED_STRING_ID
                 from TB_INDICATORS_VERSIONS ind left join TB_INTERNATIONAL_STRINGS ins on ins.ID = ind.NOTES_FK left join TB_LOCALISED_STRINGS loc on loc.INTERNATIONAL_STRING_FK = ins.ID
                 where loc.LABEL like 'null');
 
 update TB_INDICATORS_VERSIONS indver
 set NOTES_FK = null
-where indver.NOTES_FK = (select ins.ID
+where indver.NOTES_FK in (select ins.ID
                             from TB_INTERNATIONAL_STRINGS ins
                             where ins.ID NOT IN (select loc.INTERNATIONAL_STRING_FK
                                                 from TB_LOCALISED_STRINGS loc));
@@ -102,13 +102,13 @@ where indver.NOTES_FK = (select ins.ID
 -- ..................................................................................................................................
 
 delete from TB_LOCALISED_STRINGS loc
-where loc.ID = (select loc.ID as LOCALISED_STRING_ID
+where loc.ID in (select loc.ID as LOCALISED_STRING_ID
                 from TB_LIS_UNITS_MULTIPLIERS ind left join TB_INTERNATIONAL_STRINGS ins on ins.ID = ind.TITLE_FK left join TB_LOCALISED_STRINGS loc on loc.INTERNATIONAL_STRING_FK = ins.ID
                 where loc.LABEL like 'null');
 
 update TB_LIS_UNITS_MULTIPLIERS indver
 set TITLE_FK = null
-where indver.TITLE_FK = (select ins.ID
+where indver.TITLE_FK in (select ins.ID
                             from TB_INTERNATIONAL_STRINGS ins
                             where ins.ID NOT IN (select loc.INTERNATIONAL_STRING_FK
                                                 from TB_LOCALISED_STRINGS loc));
@@ -118,13 +118,13 @@ where indver.TITLE_FK = (select ins.ID
 -- ..................................................................................................................................
 
 delete from TB_LOCALISED_STRINGS loc
-where loc.ID = (select loc.ID as LOCALISED_STRING_ID
+where loc.ID in (select loc.ID as LOCALISED_STRING_ID
                 from TB_LIS_QUANTITIES_UNITS ind left join TB_INTERNATIONAL_STRINGS ins on ins.ID = ind.TITLE_FK left join TB_LOCALISED_STRINGS loc on loc.INTERNATIONAL_STRING_FK = ins.ID
                 where loc.LABEL like 'null');
 
 update TB_LIS_QUANTITIES_UNITS indver
 set TITLE_FK = null
-where indver.TITLE_FK = (select ins.ID
+where indver.TITLE_FK in (select ins.ID
                             from TB_INTERNATIONAL_STRINGS ins
                             where ins.ID NOT IN (select loc.INTERNATIONAL_STRING_FK
                                                 from TB_LOCALISED_STRINGS loc));
@@ -134,13 +134,13 @@ where indver.TITLE_FK = (select ins.ID
 -- ..................................................................................................................................
 
 delete from TB_LOCALISED_STRINGS loc
-where loc.ID = (select loc.ID as LOCALISED_STRING_ID
+where loc.ID in (select loc.ID as LOCALISED_STRING_ID
                 from TB_LIS_GEOGR_GRANULARITIES ind left join TB_INTERNATIONAL_STRINGS ins on ins.ID = ind.TITLE_FK left join TB_LOCALISED_STRINGS loc on loc.INTERNATIONAL_STRING_FK = ins.ID
                 where loc.LABEL like 'null');
 
 update TB_LIS_GEOGR_GRANULARITIES indver
 set TITLE_FK = null
-where indver.TITLE_FK = (select ins.ID
+where indver.TITLE_FK in (select ins.ID
                             from TB_INTERNATIONAL_STRINGS ins
                             where ins.ID NOT IN (select loc.INTERNATIONAL_STRING_FK
                                                 from TB_LOCALISED_STRINGS loc));
@@ -150,13 +150,13 @@ where indver.TITLE_FK = (select ins.ID
 -- ..................................................................................................................................
 
 delete from TB_LOCALISED_STRINGS loc
-where loc.ID = (select loc.ID as LOCALISED_STRING_ID
+where loc.ID in (select loc.ID as LOCALISED_STRING_ID
                 from TB_LIS_GEOGR_VALUES ind left join TB_INTERNATIONAL_STRINGS ins on ins.ID = ind.TITLE_FK left join TB_LOCALISED_STRINGS loc on loc.INTERNATIONAL_STRING_FK = ins.ID
                 where loc.LABEL like 'null');
 
 update TB_LIS_GEOGR_VALUES indver
 set TITLE_FK = null
-where indver.TITLE_FK = (select ins.ID
+where indver.TITLE_FK in (select ins.ID
                             from TB_INTERNATIONAL_STRINGS ins
                             where ins.ID NOT IN (select loc.INTERNATIONAL_STRING_FK
                                                 from TB_LOCALISED_STRINGS loc));
@@ -165,13 +165,13 @@ where indver.TITLE_FK = (select ins.ID
 -- ..................................................................................................................................
 
 delete from TB_LOCALISED_STRINGS loc
-where loc.ID = (select loc.ID as LOCALISED_STRING_ID
+where loc.ID in (select loc.ID as LOCALISED_STRING_ID
                 from TB_LIS_GEOGR_VALUES ind left join TB_INTERNATIONAL_STRINGS ins on ins.ID = ind.TITLE_FK left join TB_LOCALISED_STRINGS loc on loc.INTERNATIONAL_STRING_FK = ins.ID
                 where loc.LABEL like 'null');
 
 update TB_LIS_GEOGR_VALUES indver
 set TITLE_FK = null
-where indver.TITLE_FK = (select ins.ID
+where indver.TITLE_FK in (select ins.ID
                             from TB_INTERNATIONAL_STRINGS ins
                             where ins.ID NOT IN (select loc.INTERNATIONAL_STRING_FK
                                                 from TB_LOCALISED_STRINGS loc));
@@ -181,13 +181,13 @@ where indver.TITLE_FK = (select ins.ID
 -- ..................................................................................................................................
 
 delete from TB_LOCALISED_STRINGS loc
-where loc.ID = (select loc.ID as LOCALISED_STRING_ID
+where loc.ID in (select loc.ID as LOCALISED_STRING_ID
                 from TB_QUANTITIES ind left join TB_INTERNATIONAL_STRINGS ins on ins.ID = ind.PERCENTAGE_OF_FK left join TB_LOCALISED_STRINGS loc on loc.INTERNATIONAL_STRING_FK = ins.ID
                 where loc.LABEL like 'null');
 
 update TB_QUANTITIES indver
 set PERCENTAGE_OF_FK = null
-where indver.PERCENTAGE_OF_FK = (select ins.ID
+where indver.PERCENTAGE_OF_FK in (select ins.ID
                             from TB_INTERNATIONAL_STRINGS ins
                             where ins.ID NOT IN (select loc.INTERNATIONAL_STRING_FK
                                                 from TB_LOCALISED_STRINGS loc));
@@ -197,13 +197,13 @@ where indver.PERCENTAGE_OF_FK = (select ins.ID
 -- ..................................................................................................................................
 
 delete from TB_LOCALISED_STRINGS loc
-where loc.ID = (select loc.ID as LOCALISED_STRING_ID
+where loc.ID in (select loc.ID as LOCALISED_STRING_ID
                 from TB_DATA_SOURCES ind left join TB_INTERNATIONAL_STRINGS ins on ins.ID = ind.SOURCE_SURVEY_TITLE_FK left join TB_LOCALISED_STRINGS loc on loc.INTERNATIONAL_STRING_FK = ins.ID
                 where loc.LABEL like 'null');
 
 update TB_DATA_SOURCES indver
 set SOURCE_SURVEY_TITLE_FK = null
-where indver.SOURCE_SURVEY_TITLE_FK = (select ins.ID
+where indver.SOURCE_SURVEY_TITLE_FK in (select ins.ID
                             from TB_INTERNATIONAL_STRINGS ins
                             where ins.ID NOT IN (select loc.INTERNATIONAL_STRING_FK
                                                 from TB_LOCALISED_STRINGS loc));
@@ -212,13 +212,13 @@ where indver.SOURCE_SURVEY_TITLE_FK = (select ins.ID
 -- ..................................................................................................................................
 
 delete from TB_LOCALISED_STRINGS loc
-where loc.ID = (select loc.ID as LOCALISED_STRING_ID
+where loc.ID in (select loc.ID as LOCALISED_STRING_ID
                 from TB_DATA_SOURCES ind left join TB_INTERNATIONAL_STRINGS ins on ins.ID = ind.SOURCE_SURVEY_ACRONYM_FK left join TB_LOCALISED_STRINGS loc on loc.INTERNATIONAL_STRING_FK = ins.ID
                 where loc.LABEL like 'null');
 
 update TB_DATA_SOURCES indver
 set SOURCE_SURVEY_ACRONYM_FK = null
-where indver.SOURCE_SURVEY_ACRONYM_FK = (select ins.ID
+where indver.SOURCE_SURVEY_ACRONYM_FK in (select ins.ID
                             from TB_INTERNATIONAL_STRINGS ins
                             where ins.ID NOT IN (select loc.INTERNATIONAL_STRING_FK
                                                 from TB_LOCALISED_STRINGS loc));
@@ -227,13 +227,13 @@ where indver.SOURCE_SURVEY_ACRONYM_FK = (select ins.ID
 -- ..................................................................................................................................
 
 delete from TB_LOCALISED_STRINGS loc
-where loc.ID = (select loc.ID as LOCALISED_STRING_ID
+where loc.ID in (select loc.ID as LOCALISED_STRING_ID
                 from TB_INDICATORS_INSTANCES ind left join TB_INTERNATIONAL_STRINGS ins on ins.ID = ind.TITLE_FK left join TB_LOCALISED_STRINGS loc on loc.INTERNATIONAL_STRING_FK = ins.ID
                 where loc.LABEL like 'null');
 
 update TB_INDICATORS_INSTANCES indver
 set TITLE_FK = null
-where indver.TITLE_FK = (select ins.ID
+where indver.TITLE_FK in (select ins.ID
                             from TB_INTERNATIONAL_STRINGS ins
                             where ins.ID NOT IN (select loc.INTERNATIONAL_STRING_FK
                                                 from TB_LOCALISED_STRINGS loc));
@@ -244,13 +244,13 @@ where indver.TITLE_FK = (select ins.ID
 -- ..................................................................................................................................
 
 delete from TB_LOCALISED_STRINGS loc
-where loc.ID = (select loc.ID as LOCALISED_STRING_ID
+where loc.ID in (select loc.ID as LOCALISED_STRING_ID
                 from TB_DIMENSIONS ind left join TB_INTERNATIONAL_STRINGS ins on ins.ID = ind.TITLE_FK left join TB_LOCALISED_STRINGS loc on loc.INTERNATIONAL_STRING_FK = ins.ID
                 where loc.LABEL like 'null');
 
 update TB_DIMENSIONS indver
 set TITLE_FK = null
-where indver.TITLE_FK = (select ins.ID
+where indver.TITLE_FK in (select ins.ID
                             from TB_INTERNATIONAL_STRINGS ins
                             where ins.ID NOT IN (select loc.INTERNATIONAL_STRING_FK
                                                 from TB_LOCALISED_STRINGS loc));
@@ -259,13 +259,13 @@ where indver.TITLE_FK = (select ins.ID
 -- ..................................................................................................................................
 
 delete from TB_LOCALISED_STRINGS loc
-where loc.ID = (select loc.ID as LOCALISED_STRING_ID
+where loc.ID in (select loc.ID as LOCALISED_STRING_ID
                 from TB_TRANSLATIONS ind left join TB_INTERNATIONAL_STRINGS ins on ins.ID = ind.TITLE_FK left join TB_LOCALISED_STRINGS loc on loc.INTERNATIONAL_STRING_FK = ins.ID
                 where loc.LABEL like 'null');
 
 update TB_TRANSLATIONS indver
 set TITLE_FK = null
-where indver.TITLE_FK = (select ins.ID
+where indver.TITLE_FK in (select ins.ID
                             from TB_INTERNATIONAL_STRINGS ins
                             where ins.ID NOT IN (select loc.INTERNATIONAL_STRING_FK
                                                 from TB_LOCALISED_STRINGS loc));
@@ -275,13 +275,13 @@ where indver.TITLE_FK = (select ins.ID
 -- ..................................................................................................................................
 
 delete from TB_LOCALISED_STRINGS loc
-where loc.ID = (select loc.ID as LOCALISED_STRING_ID
+where loc.ID in (select loc.ID as LOCALISED_STRING_ID
                 from TB_TRANSLATIONS ind left join TB_INTERNATIONAL_STRINGS ins on ins.ID = ind.TITLE_SUMMARY_FK left join TB_LOCALISED_STRINGS loc on loc.INTERNATIONAL_STRING_FK = ins.ID
                 where loc.LABEL like 'null');
 
 update TB_TRANSLATIONS indver
 set TITLE_SUMMARY_FK = null
-where indver.TITLE_SUMMARY_FK = (select ins.ID
+where indver.TITLE_SUMMARY_FK in (select ins.ID
                             from TB_INTERNATIONAL_STRINGS ins
                             where ins.ID NOT IN (select loc.INTERNATIONAL_STRING_FK
                                                 from TB_LOCALISED_STRINGS loc));
@@ -291,13 +291,13 @@ where indver.TITLE_SUMMARY_FK = (select ins.ID
 -- ..................................................................................................................................
 
 delete from TB_LOCALISED_STRINGS loc
-where loc.ID = (select loc.ID as LOCALISED_STRING_ID
+where loc.ID in (select loc.ID as LOCALISED_STRING_ID
                 from TB_EXTERNAL_ITEMS ind left join TB_INTERNATIONAL_STRINGS ins on ins.ID = ind.TITLE_FK left join TB_LOCALISED_STRINGS loc on loc.INTERNATIONAL_STRING_FK = ins.ID
                 where loc.LABEL like 'null');
 
 update TB_EXTERNAL_ITEMS indver
 set TITLE_FK = null
-where indver.TITLE_FK = (select ins.ID
+where indver.TITLE_FK in (select ins.ID
                             from TB_INTERNATIONAL_STRINGS ins
                             where ins.ID NOT IN (select loc.INTERNATIONAL_STRING_FK
                                                 from TB_LOCALISED_STRINGS loc));
