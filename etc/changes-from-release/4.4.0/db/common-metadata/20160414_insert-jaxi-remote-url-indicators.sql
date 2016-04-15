7ae78f4f8853d120b068a746c4ee9da76bfe3c4a
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2016-04-15 12:27:46 +0000

    INDISTAC-981 - Añadir el botón de previsualizar datos al entorno de difusión
    > Ejemplo de cumplimentación de propiedad en script de inserción de datos.
    
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@55148 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- etc/changes-from-release/4.4.0/db/common-metadata/20160414_insert-jaxi-remote-url-indicators.sql
+++ etc/changes-from-release/4.4.0/db/common-metadata/20160414_insert-jaxi-remote-url-indicators.sql
@@ -1,7 +1,10 @@
 --------------------------------------------------------------------------------
 -- INDISTAC-981 - Añadir el botón de previsualizar datos al entorno de difusión
 --------------------------------------------------------------------------------
-insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE) values(GET_NEXT_SEQUENCE_VALUE('DATA_CONFIGURATIONS'),1,1,'indicators.jaxi.remote.url.indicator','FILL_ME');
+
+-- IMPORTANTE modificar el FILL_ME. 
+-- Ejemplo de valor: http://estadisticas.arte-consultores.com/jaxi-ciber/tabla.do?indicador=[INDICATOR]
+insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE) values(GET_NEXT_SEQUENCE_VALUE('DATA_CONFIGURATIONS'),1,1,'indicators.jaxi.remote.url.indicator','FILL_ME/tabla.do?indicador=[INDICATOR]');
 UPDATE TB_SEQUENCES SET SEQUENCE_NEXT_VALUE = SEQUENCE_NEXT_VALUE + 1 WHERE SEQUENCE_NAME = 'DATA_CONFIGURATIONS';
 
 commit;
