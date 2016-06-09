6aaa626976efbc0dbd552f2ce3e1babd41c947a0
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2016-06-09 11:08:39 +0000

    METAMAC-2452 - Los archivos urlrewrite de las aplicaciones externas no mantienen el contexto de la aplicación
    > Indicators
    
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/indicators/trunk/indicators@55422 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- indicators-external-api-web/src/main/java/es/gobcan/istac/indicators/web/diffusion/urlrewrite/IndicatorsRewriteMatch.java
+++ indicators-external-api-web/src/main/java/es/gobcan/istac/indicators/web/diffusion/urlrewrite/IndicatorsRewriteMatch.java
@@ -1,30 +1,49 @@
 package es.gobcan.istac.indicators.web.diffusion.urlrewrite;
 
+import static org.apache.commons.lang.StringUtils.EMPTY;
+import static org.apache.commons.lang.StringUtils.isBlank;
+import static org.siemac.metamac.core.common.constants.CoreCommonConstants.API_LATEST;
+import static org.siemac.metamac.core.common.constants.CoreCommonConstants.URL_SEPARATOR;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.siemac.metamac.core.common.conf.ConfigurationService;
 import org.siemac.metamac.core.common.exception.MetamacException;
 import org.siemac.metamac.core.common.util.ApplicationContextProvider;
-import org.siemac.metamac.core.common.util.urlrewrite.AbstractRewriteMatch;
+import org.tuckey.web.filters.urlrewrite.extend.RewriteMatch;
 
 import es.gobcan.istac.indicators.rest.IndicatorsRestConstants;
 
-class IndicatorsRewriteMatch extends AbstractRewriteMatch {
+class IndicatorsRewriteMatch extends RewriteMatch {
+
+    private Pattern              urlPattern           = Pattern.compile(".*/api/indicators/" + API_LATEST + "(/(.*)?)?");
 
     private ConfigurationService configurationService = null;
 
     @Override
-    protected String[] getAcceptedApiPrefixes() {
-        return new String[]{"indicators"};
+    public boolean execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String requestURI = ((HttpServletRequest) request).getRequestURI();
+        String queryString = ((HttpServletRequest) request).getQueryString();
+        Matcher matcher = urlPattern.matcher(requestURI);
+        if (matcher.matches() && matcher.groupCount() > 1) {
+            String requestPathAfterVersion = matcher.group(1);
+            String location = buildTargetLocation(IndicatorsRestConstants.API_VERSION_1_0, requestPathAfterVersion, queryString);
+            response.sendRedirect(location);
+        }
+        return true;
     }
 
-    @Override
-    protected String getLatestApiVersion() {
-        return IndicatorsRestConstants.API_VERSION_1_0;
+    private String buildTargetLocation(String apiVersion, String requestPathAfterVersion, String queryString) throws ServletException {
+        return getApiBaseUrl() + URL_SEPARATOR + apiVersion + (requestPathAfterVersion == null ? URL_SEPARATOR : requestPathAfterVersion) + (isBlank(queryString) ? EMPTY : "?" + queryString);
     }
 
-    @Override
-    protected String getApiBaseUrl() throws ServletException {
+    private String getApiBaseUrl() throws ServletException {
         try {
             return getConfigurationService().retrieveIndicatorsExternalApiUrlBase();
         } catch (MetamacException e) {
