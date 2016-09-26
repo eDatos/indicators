1a8ebcd808ad49afb9c1518e736fef88b57cfb1f
acidcar <acidcar@arte-consultores.com>
2016-09-26 12:38:25 +0100

    METAMAC-2506 - Automatizar la creación de los paquetes de instalación > Igualando rutas

--- etc/db/indicators-data/01-create/03-sequences-oracle.sql
+++
@@ -1,7 +0,0 @@
-create sequence SEQ_DATASETS;
-create sequence SEQ_DASET_DIMS;
-create sequence SEQ_DATASET_ATTRIBUTES;
-create sequence SEQ_ATTRIBUTES;
-create sequence SEQ_ATTR_DIMS;
-create sequence SEQ_I18NSTRS;
-create sequence SEQ_L10NSTRS;
--- etc/db/indicators-data/01-create/05-roles-oracle.sql
+++
@@ -1,6 +0,0 @@
--- It has to be executed with enough priviligies
--- The role has to be same specified in DATA
-CREATE ROLE <FILL_ME_WITH_ROLE_NAME>;
-
--- Example for assign role to user
--- GRANT INDICATORS_DATA TO <USERNAME>;
