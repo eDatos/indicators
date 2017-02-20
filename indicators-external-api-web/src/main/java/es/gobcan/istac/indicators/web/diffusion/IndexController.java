7dcc4c2a7db787ffa08924f26e320ae1c29e3f7a
Francisco Javier Rodríguez García <frodgar@arte-consultores.com>
2017-02-20 12:45:50 +0000

    INDISTAC-1001 - Elaborar la página de listado de versiones de la API

--- indicators-external-api-web/src/main/java/es/gobcan/istac/indicators/web/diffusion/IndexController.java
+++ indicators-external-api-web/src/main/java/es/gobcan/istac/indicators/web/diffusion/IndexController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class IndexController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = {"/", "/api/indicators"}, method = RequestMethod.GET)
     public String index() {
         return "index";
     }
