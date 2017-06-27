dc587aa0769d2e509b1d75cad7bdd74b2935b36a
acidcar <acidcar@arte-consultores.com>
2017-06-27 14:26:51 +0100

    METAMAC-2624 - Crear tarea para despliegue de todo metamac

--- etc/deploy/deploy.sh
+++ etc/deploy/deploy.sh
@@ -5,6 +5,11 @@ TRANSFER_PATH=$HOME_PATH/tmp
 DEPLOY_TARGET_PATH=/servers/metamac/tomcats/metamac01/webapps
 ENVIRONMENT_RELATIVE_PATH_FILE=WEB-INF/classes/indicators/environment.xml
 LOGBACK_RELATIVE_PATH_FILE=WEB-INF/classes/logback.xml
+RESTART=1
+
+if [ "$1" == "--no-restart" ]; then
+    RESTART=0
+fi
 
 scp -r etc/deploy deploy@estadisticas.arte-consultores.com:$TRANSFER_PATH
 scp indicators-internal-web/target/indicators-internal-*.war deploy@estadisticas.arte-consultores.com:$TRANSFER_PATH/indicators-internal.war
@@ -14,35 +19,37 @@ ssh deploy@estadisticas.arte-consultores.com <<EOF
 
     chmod a+x $TRANSFER_PATH/deploy/*.sh;
     . $TRANSFER_PATH/deploy/utilities.sh
-    
-    sudo service metamac01 stop
-    checkPROC "metamac"
-    
+
+    if [ $RESTART -eq 1 ]; then
+        sudo service metamac01 stop
+        checkPROC "metamac"
+    fi
+
     ###
     # INDICATORS-INTERNAL
     ###
-    
+
     # Update Process
     sudo rm -rf $DEPLOY_TARGET_PATH/indicators-internal
     sudo mv $TRANSFER_PATH/indicators-internal.war $DEPLOY_TARGET_PATH/indicators-internal.war
     sudo unzip $DEPLOY_TARGET_PATH/indicators-internal.war -d $DEPLOY_TARGET_PATH/indicators-internal
     sudo rm -rf $DEPLOY_TARGET_PATH/indicators-internal.war
-    
+
     # Restore Configuration
     sudo cp $HOME_PATH/environment_internal.xml $DEPLOY_TARGET_PATH/indicators-internal/$ENVIRONMENT_RELATIVE_PATH_FILE
     sudo cp $HOME_PATH/logback_internal.xml $DEPLOY_TARGET_PATH/indicators-internal/$LOGBACK_RELATIVE_PATH_FILE
-    
-    
+
+
     ###
     # INDICATORS-EXTERNAL
     ###
-    
+
     # Update Process
     sudo rm -rf $DEPLOY_TARGET_PATH/indicators
     sudo mv $TRANSFER_PATH/indicators.war $DEPLOY_TARGET_PATH/indicators.war
     sudo unzip $DEPLOY_TARGET_PATH/indicators.war -d $DEPLOY_TARGET_PATH/indicators
     sudo rm -rf $DEPLOY_TARGET_PATH/indicators.war
-    
+
     # Restore Configuration
     sudo cp $HOME_PATH/environment.xml $DEPLOY_TARGET_PATH/indicators/$ENVIRONMENT_RELATIVE_PATH_FILE
     sudo cp $HOME_PATH/logback.xml $DEPLOY_TARGET_PATH/indicators/$LOGBACK_RELATIVE_PATH_FILE
@@ -56,18 +63,18 @@ ssh deploy@estadisticas.arte-consultores.com <<EOF
     sudo mv $TRANSFER_PATH/indicators-visualizations.war $DEPLOY_TARGET_PATH/indicators-visualizations.war
     sudo unzip $DEPLOY_TARGET_PATH/indicators-visualizations.war -d $DEPLOY_TARGET_PATH/indicators-visualizations
     sudo rm -rf $DEPLOY_TARGET_PATH/indicators-visualizations.war
-    
+
     # Restore Configuration
     sudo cp $HOME_PATH/environment_visualizations.xml $DEPLOY_TARGET_PATH/indicators-visualizations/$ENVIRONMENT_RELATIVE_PATH_FILE
     sudo cp $HOME_PATH/logback_visualizations.xml $DEPLOY_TARGET_PATH/indicators-visualizations/$LOGBACK_RELATIVE_PATH_FILE
 
-    
-    sudo chown -R metamac.metamac /servers/metamac
-    sudo service metamac01 start
+    if [ $RESTART -eq 1 ]; then
+        sudo chown -R metamac.metamac /servers/metamac
+        sudo service metamac01 start
+    fi
 
     echo "Finished deploy"
-#    checkURL "http://estadisticas.arte-consultores.com/indicators-internal/" "metamac01"
-#    checkURL "http://estadisticas.arte-consultores.com/indicators/v1.0/~latest" "metamac01"
-#    checkURL "http://estadisticas.arte-consultores.com/indicators-visualizations/indicatorsSystems" "metamac01"
-
+    #    checkURL "http://estadisticas.arte-consultores.com/indicators-internal/" "metamac01"
+    #    checkURL "http://estadisticas.arte-consultores.com/indicators/v1.0/~latest" "metamac01"
+    #    checkURL "http://estadisticas.arte-consultores.com/indicators-visualizations/indicatorsSystems" "metamac01"
 EOF
