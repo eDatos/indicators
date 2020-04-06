a97314673d232e3a158e9c152ba000b3b7da55c9
Miguel Jonás Bravo García <mbragar@arte-consultores.com>
2020-04-06 10:30:44 +0100

    EDATOS-3146 - Instalación del entorno de estadísticas en la nueva máquina OVH
    > Modificaciones en el fichero deploy.sh para tener en cuenta el doble salto jenkins-estadisticas-maquina metamac
    > No es necesario restaurar el fichero logback dado que está configurado externamente en el fichero applicationContext.xml

--- etc/deploy/deploy.sh
+++ etc/deploy/deploy.sh
@@ -12,11 +12,11 @@ if [ "$1" == "--no-restart" ]; then
     RESTART=0
 fi
 
-scp -r etc/deploy deploy@estadisticas.arte-consultores.com:$TRANSFER_PATH
-scp indicators-internal-web/target/indicators-internal-*.war deploy@estadisticas.arte-consultores.com:$TRANSFER_PATH/indicators-internal.war
-scp indicators-web/target/indicators-*.war deploy@estadisticas.arte-consultores.com:$TRANSFER_PATH/indicators-visualizations.war
-scp indicators-external-api-web/target/indicators-*.war deploy@estadisticas.arte-consultores.com:$TRANSFER_PATH/indicators.war
-ssh deploy@estadisticas.arte-consultores.com <<EOF
+scp -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" -r etc/deploy deploy@192.168.10.16:$TRANSFER_PATH
+scp -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" indicators-internal-web/target/indicators-internal-*.war deploy@192.168.10.16:$TRANSFER_PATH/indicators-internal.war
+scp -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" indicators-web/target/indicators-*.war deploy@192.168.10.16:$TRANSFER_PATH/indicators-visualizations.war
+scp -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" indicators-external-api-web/target/indicators-*.war deploy@192.168.10.16:$TRANSFER_PATH/indicators.war
+ssh -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" deploy@192.168.10.16 <<EOF
 
     chmod a+x $TRANSFER_PATH/deploy/*.sh;
     . $TRANSFER_PATH/deploy/utilities.sh
@@ -38,7 +38,8 @@ ssh deploy@estadisticas.arte-consultores.com <<EOF
 
     # Restore Configuration
     sudo cp $HOME_PATH/environment_internal.xml $DEPLOY_TARGET_PATH_INTERNAL/indicators-internal/$ENVIRONMENT_RELATIVE_PATH_FILE
-    sudo cp $HOME_PATH/logback_internal.xml $DEPLOY_TARGET_PATH_INTERNAL/indicators-internal/$LOGBACK_RELATIVE_PATH_FILE
+    # Take care!, it's not necessary to restore the logback.xml file, it's externally configured in the applicationContext.xml file
+    # sudo cp $HOME_PATH/logback_internal.xml $DEPLOY_TARGET_PATH_INTERNAL/indicators-internal/$LOGBACK_RELATIVE_PATH_FILE
     
     if [ $RESTART -eq 1 ]; then
         sudo chown -R edatos-internal.edatos-internal /servers/edatos-internal     
