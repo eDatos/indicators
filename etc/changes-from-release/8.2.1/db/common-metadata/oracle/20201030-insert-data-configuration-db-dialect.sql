ed1d0e31ff18198b96d96c1f6d004866981a9e00
Miguel Jonás Bravo García <mbragar@arte-consultores.com>
2021-01-19 11:40:05 +0000

    EDATOS-3197 - Compatibilidad con PostgreSQL
    > Se corrige errata en los scripts de BBDD con respecto al dialecto Oracle a utilizar para permitir que la aplicación despliegue correctamente en entornos con base de datos Oracle

--- etc/changes-from-release/8.2.1/db/common-metadata/oracle/20201030-insert-data-configuration-db-dialect.sql
+++ etc/changes-from-release/8.2.1/db/common-metadata/oracle/20201030-insert-data-configuration-db-dialect.sql
@@ -2,10 +2,10 @@
 -- EDATOS-3197 - Compatibilidad con PostgreSQL
 -- -----------------------------------------------------------------------------------------------
 
-insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE) values(GET_NEXT_SEQUENCE_VALUE('DATA_CONFIGURATIONS'),1,1,'indicators.core.db.dialect','org.hibernate.dialect.Oracle10gDialect');
+insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE) values(GET_NEXT_SEQUENCE_VALUE('DATA_CONFIGURATIONS'),1,1,'indicators.core.db.dialect','org.siemac.metamac.hibernate.dialect.Oracle10gDialectMetamac');
 UPDATE TB_SEQUENCES SET SEQUENCE_NEXT_VALUE = SEQUENCE_NEXT_VALUE + 1 WHERE SEQUENCE_NAME = 'DATA_CONFIGURATIONS';
 
-insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE) values(GET_NEXT_SEQUENCE_VALUE('DATA_CONFIGURATIONS'),1,1,'indicators.dsrepo.db.dialect','org.hibernate.dialect.Oracle10gDialect');
+insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE) values(GET_NEXT_SEQUENCE_VALUE('DATA_CONFIGURATIONS'),1,1,'indicators.dsrepo.db.dialect','org.siemac.metamac.hibernate.dialect.Oracle10gDialectMetamac');
 UPDATE TB_SEQUENCES SET SEQUENCE_NEXT_VALUE = SEQUENCE_NEXT_VALUE + 1 WHERE SEQUENCE_NAME = 'DATA_CONFIGURATIONS';
 
 commit;
