cf38b7c4bffc5a1566bef8f43a34329e987266e7
Francisco Javier Rodríguez García <frodgar@arte-consultores.com>
2021-04-09 12:24:46 +0100

    EDATOS-3043 - Indicadores ha de usar la cabecera del portal definida en las propiedades del common-metadata

--- indicators-core/src/main/java/es/gobcan/istac/indicators/core/util/FreeMarkerUtil.java
+++ indicators-core/src/main/java/es/gobcan/istac/indicators/core/util/FreeMarkerUtil.java
@@ -8,6 +8,9 @@ import java.net.URL;
 
 import org.apache.commons.lang.StringUtils;
 
+/* Even when we opted for this approach, there are others if neccesary, see
+ * http://freemarker.624813.n4.nabble.com/can-I-include-quot-http-some-page-way-over-here-html-quot-td624983.html
+ */
 public class FreeMarkerUtil {
 
     private static final String UTF8_HTML_ENCODING = "UTF-8";
