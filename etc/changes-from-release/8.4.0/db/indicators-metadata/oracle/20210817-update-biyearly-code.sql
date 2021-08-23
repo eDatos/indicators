2f4777def05deaa4ed8f5861000734be87e65c08
Miguel Jonás Bravo García <mbragar@arte-consultores.com>
2021-08-23 08:01:33 +0100

    EDATOS-3037 - Problemas varios asociados al código de MetamacTimeUtils
    > Se corrige errata en los scripts SQL

--- etc/changes-from-release/8.4.0/db/indicators-metadata/oracle/20210817-update-biyearly-code.sql
+++ etc/changes-from-release/8.4.0/db/indicators-metadata/oracle/20210817-update-biyearly-code.sql
@@ -2,7 +2,7 @@
 -- EDATOS-3037 - Problemas varios asociados al código de MetamacTimeUtils
 -- --------------------------------------------------------------------------------------------------
 
-UPDATE TB_TRANSLATIONS SET CODE = 'TIME_VALUE.BIYEARLY.H1' where code = 'TIME_VALUE.BIYEARLY.S1';
-UPDATE TB_TRANSLATIONS SET CODE = 'TIME_VALUE.BIYEARLY.H2' where code = 'TIME_VALUE.BIYEARLY.S2';
+UPDATE TB_TRANSLATIONS SET CODE = 'TIME_VALUE.BIYEARLY.S1' where code = 'TIME_VALUE.BIYEARLY.H1';
+UPDATE TB_TRANSLATIONS SET CODE = 'TIME_VALUE.BIYEARLY.S2' where code = 'TIME_VALUE.BIYEARLY.H2';
 
 commit;
